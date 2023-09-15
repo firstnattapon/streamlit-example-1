c8b53bdb23c1c8801e1b038041740ad6b98e82c8
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-09-15 17:05:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np
 
+st.write("สมการ" ,"  =  -742+1500"  , "fix 1500 : Initial Port" , "เริ่ม 6.88") 
+st.write("")
 x = st.number_input('ราคา')
 y = -742+1500 * np.log(x)
-st.write("สมการ" ,"  =  -742+1500"  , "fix 1500 : Initial Port" , "เริ่ม 6.88") 
-st.write( y) 
-
-
+st.write("Price", x ,"=" , y) 
