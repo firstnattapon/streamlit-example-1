b04c6d01be153966b1ccc9d9a35623041f2de515
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-10-21 22:43:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,23 +12,26 @@ y = -742+1500 * np.log(x)
 st.write("Price", x ,"=" , y) 
 st.write( 'cf' ,"=", p - y) 
 
+button_NEW = st.button("NEW_DATA")
+if button_NEW:
+    np.save('my_array.npy', np.array([]))
+    st.write( "NEW_DATA")
 
-def save_numpy_array(array, filename):
-    np.save(filename, array)
+button_LOAD = st.button("LOAD_DATA")
+if button_LOAD:
+    my_array = np.load('my_array.npy')
+    st.write( my_array) 
 
-st.title("Save Numpy Array to File")
-
-array = np.random.randint(0, 100, size=(100,))
-
-button = st.button("Save Array")
-
-if button:
-    filename = "my_array.npy"
-    save_numpy_array(array, filename)
-
-    st.write("Array saved to file: {}".format(filename))
-
-
-my_array = np.load('my_array.npy')
-
-st.write( my_array) 
+button_ADD = st.button("ADD_CF")
+if button_ADD:
+    my_array_a = np.load('my_array.npy')
+    my_array_b = np.append(my_array_a, p - y)
+    np.save('my_array.npy', my_array_b)
+    st.write( button_ADD)
+    
+button_DEL = st.button("DEL_CF")
+if button_DEL:
+    my_array_a = np.load('my_array.npy')
+    my_array_b = np.delete(my_array_a, -1)
+    np.save('my_array.npy', my_array_b)
+    st.write( button_DEL)
