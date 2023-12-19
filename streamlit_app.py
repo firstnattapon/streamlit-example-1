dba67f387d779cb80a4887b5cacdb1da827eec18
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-19 21:38:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,31 +23,34 @@ col1, col2, col3,  col4, col5, col6 = st.columns(6)
 
 re = col6.button("Rerun")
 if re :
-       # np.random.seed(1074)
-       # data = np.random.randint(2, size=500)[-251:]
-       data = [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1,
-              0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1,
-              0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
-              1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0,
-              0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0,
-              0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0,
-              0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1,
-              0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0,
-              1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0,
-              1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1,
-              0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1,
-              1, 1, 1, 0, 1, 0, 0, 1, 1]
+        # np.random.seed(1074)
+        # data = np.random.randint(2, size=500)[-251:]
+        data = [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1,
+                0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1,
+                0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+                1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0,
+                0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0,
+                0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0,
+                0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1,
+                0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0,
+                1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0,
+                1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1,
+                0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1,
+                1, 1, 1, 0, 1, 0, 0, 1, 1]
+
+        tickerData = yf.Ticker( 'FFWM')
+        tickerData = tickerData.history(period= 'max' ,  start='2023-12-19')[['Close']]
+        tickerData = round(tickerData , 3)
+        tickerData.index = tickerData.index.tz_convert(tz='Asia/bangkok')
+        tickerData['Action'] = data[: len(tickerData)]
+        
+        tickerData_1 = pd.DataFrame(columns=(tickerData.columns))
+        tickerData_1['Action'] = data[len(tickerData) : len(tickerData)+3] 
+        tickerData_1.index = ['+1' , "+2" , "+3"]
        
-       tickerData = yf.Ticker( 'FFWM')
-       tickerData = tickerData.history(period= 'max' ,  start='2023-12-19')[['Close']]
-       tickerData = round(tickerData , 3)
-       tickerData.index = tickerData.index.tz_convert(tz='Asia/bangkok')
-       tickerData['Action'] = data[: len(tickerData)]
-       
-       tickerData_1 = pd.DataFrame(columns=(tickerData.columns))
-       tickerData_1['Action'] = data[len(tickerData) : len(tickerData)+3] 
-       tickerData_1.index = ['+1' , "+2" , "+3"]
-       
-       df = pd.concat([tickerData, tickerData_1], axis=0).fillna("")
-       st.table(df)
-       st.stop()
+        df = pd.concat([tickerData, tickerData_1], axis=0).fillna("")
+        st.table(df)
+        col1, col2, col3,  col4, col5, col6 = st.columns(6)
+        col3..write(tickerData.Close)
+
+        st.stop()
