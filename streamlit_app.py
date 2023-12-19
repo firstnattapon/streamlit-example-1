19e7c38f2ede3671c8df1370ba916cc8ada0b3df
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-19 22:58:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,8 @@ client = thingspeak.Channel(channel_id, write_api_key , fmt='json')
 col1, col2, col3,  col4, col5, col6 ,col7  = st.columns(7)
 col4.write("FFWM")
 
-Check_ADD = st.checkbox('Add_Last.Re.Price')
-if Check_ADD :
-    x = st.number_input('Updated ')
-    button_ADD = st.button("Updated")
-    if button_ADD:
-        client.update(  {'field1': x } )
-        st.write(x)
 
 col1, col2, col3,  col4, col5, col6 ,col7  = st.columns(7)
-
 re = col7.button("Rerun")
 if re :
         # np.random.seed(1074)
@@ -59,3 +51,11 @@ if re :
         col8.write(round(((1500 * (last_v / final_js)) - 1500) , 2 ))
         st.table(df)
         st.stop()
+
+Check_ADD = st.checkbox('Add_Last.Re.Price')
+if Check_ADD :
+    x = st.number_input('Updated ')
+    button_ADD = st.button("Updated")
+    if button_ADD:
+        client.update(  {'field1': x } )
+        st.write(x)
