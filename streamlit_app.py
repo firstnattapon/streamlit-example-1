e113ef334f108d2e8d8b14675619edba579194df
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-19 22:08:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,6 @@ if re :
         col1, col2, col3,  col4, col5, col6 = st.columns(6)
         final = client.get_field_last(field='1')
     
-        st.write(dict(final))
+        st.write(final[-50])
         col3.write(tickerData.tail(1).Close.values[0])
         st.stop()
