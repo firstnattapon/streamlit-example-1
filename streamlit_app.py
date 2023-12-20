8e95a4f08bc3fbd1c77b28e066c6c04ffb547224
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-20 18:06:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,9 @@ import thingspeak
 import json
 
 
-# st.set_page_config(page_title="Monitor", page_icon="📈")
+st.set_page_config(page_title="Monitor", page_icon="📈")
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
+
 
 channel_id = 2385118
 write_api_key = 'IPSG3MMMBJEB9DY8'
