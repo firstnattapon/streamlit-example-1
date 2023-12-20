74f07297314b109b4db53bd27bc17a1f3091b5ac
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-20 19:32:08 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -33,7 +33,7 @@ if re :
         tickerData.index = tickerData.index.tz_convert(tz='Asia/bangkok')
 
         np.random.seed(68)
-        data = np.random.randint(2, size=250+len(tickerData)) 
+        data = np.random.randint(2, size=248+len(tickerData)) 
     
         tickerData['Action'] = data[: len(tickerData)]
         
