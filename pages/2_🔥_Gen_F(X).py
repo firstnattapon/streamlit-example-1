307686c8222a51f8c5ead1e74e9711e7c383a288
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-21 19:24:33 +0700

    Update 2_🔥_Gen_F(X).py

--- pages/2_🔥_Gen_F(X).py
+++ pages/2_🔥_Gen_F(X).py
@@ -114,15 +114,16 @@ def delta2(Ticker = "FFWM" , pred = 1 ,  filter_date = '2022-12-21 12:00:00+07:0
             return  final
     except:pass
 
-o = []
-
+# o = []
+container = st.container(border=True)
 
 for i in range(1):
     Ticker = 'FFWM'
     pred  = delta2(Ticker=Ticker)
     siz = len(pred)
     z = int( pred.delta.values[-1])
-    o.append(['x' ,  z])
+    # o.append(['x' ,  z])
+    container.write("x , {}".format(z))
     # print( 'x' ,  z )
     
     for i in range(2000):
@@ -130,7 +131,7 @@ for i in range(1):
         pred  = delta2(Ticker=Ticker , pred= np.random.randint(2, size= siz) )
         y = int( pred.delta.values[-1])
         if  y > z :
-            o.append([ i , y])
+            # o.append([ i , y])
+            container.write("{} , {}".format(i,y))
             z = y
             
-st.write(srt(o))
