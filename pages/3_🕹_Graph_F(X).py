21c313d9050720c58ccaee9bdc14dd97acff4f75
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-27 23:05:09 +0700

    Update 3_🕹_Graph_F(X).py

--- pages/3_🕹_Graph_F(X).py
+++ pages/3_🕹_Graph_F(X).py
@@ -129,32 +129,32 @@ container = st.container(border=True)
 all = []
 all_id = []
 
-Graph = st.checkbox('Graph_F(X)')
-if Graph :
-    re = st.button("Rerun_Graph")
-    if re :
-        for i in range(1):
-            Ticker = 'FFWM'
-            pred  = delta2(Ticker=Ticker)
-            siz = len(pred)
-            prd_x =  pred.net_pv.values
-            z = int(prd_x[-1])
-            all.append(prd_x)
+# Graph = st.checkbox('Graph_F(X)')
+# if Graph :
+#     re = st.button("Rerun_Graph")
+#     if re :
+for i in range(1):
+    Ticker = 'FFWM'
+    pred  = delta2(Ticker=Ticker)
+    siz = len(pred)
+    prd_x =  pred.net_pv.values
+    z = int(prd_x[-1])
+    all.append(prd_x)
+    all_id.append(i)
+    container.write("x , {}".format(z))
+    # print( 'x' ,  z )
+    
+    for i in range(2000):
+        np.random.seed(i)
+        pred  = delta2(Ticker=Ticker , pred= np.random.randint(2, size= siz))
+        prd_y = pred.net_pv.values
+        y = int(prd_y[-1])
+        if  y > z :
+            # print( i , y )
+            z = y
+            all.append(prd_y)
             all_id.append(i)
-            container.write("x , {}".format(z))
-            # print( 'x' ,  z )
-            
-            for i in range(2000):
-                np.random.seed(i)
-                pred  = delta2(Ticker=Ticker , pred= np.random.randint(2, size= siz))
-                prd_y = pred.net_pv.values
-                y = int(prd_y[-1])
-                if  y > z :
-                    # print( i , y )
-                    z = y
-                    all.append(prd_y)
-                    all_id.append(i)
-                    container.write("{} , {}".format(i,y))
+            container.write("{} , {}".format(i,y))
                     
 chart_data = pd.DataFrame(np.array(all).T , columns= all_id)
 st.line_chart(chart_data)
