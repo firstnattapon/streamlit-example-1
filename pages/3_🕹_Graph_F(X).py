3770114023f359a1a964f6e7db62e8c0e92607ed
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-28 19:29:55 +0700

    Update 3_🕹_Graph_F(X).py

--- pages/3_🕹_Graph_F(X).py
+++ pages/3_🕹_Graph_F(X).py
@@ -189,7 +189,7 @@ if Graph_M :
                 y = int(prd_y[-1])
                 all_m.append(prd_y)
                 all_id_m.append(i)
-                container_1.write("{} , {} , {}".format(i , y  , op/y ))
+                container_1.write("{} , {} , {}".format(i , y  ,  y/op ))
                 op = y
             chart_data = pd.DataFrame(np.array(all_m).T , columns= np.array(all_id_m))
             st.line_chart(chart_data)
