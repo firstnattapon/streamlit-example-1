c93bad7332535bd7bd32a790cd184551c0563aaa
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-28 21:45:41 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -46,7 +46,7 @@ tickerData_1.index = ['+0' , "+1" , "+2" , "+3" , "+4"]
 df = pd.concat([tickerData.tail(5), tickerData_1], axis=0).fillna("")
 st.write("f(x) {}".format(fx_js))
 st.table(df.tail(7))
-st.table(tickerData)
+st.table(tickerData.tail(7))
 
 st.write("***ก่อนตลาดเปิดตรวจสอบ TB ล่าสุด > RE เมื่อตลอดเปิด")
         # st.stop()
