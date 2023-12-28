2a8f7529486862fecc1b028e6976c9512b24a793
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-28 22:08:54 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -44,10 +44,10 @@ tickerData_1 = pd.DataFrame(columns=(tickerData.columns))
 tickerData_1['action'] = np.random.randint(2, size = len(tickerData)+6)[-5:]
 tickerData_1.index = ['+0' , "+1" , "+2" , "+3" , "+4"]
 df = pd.concat([tickerData , tickerData_1], axis=0).fillna("")
-
+np.random.seed(fx_js)
 df['re_check'] = np.random.randint(2, size = len(df))
 st.write("f(x) {}".format(fx_js))
-st.table(df.tail(7))
+st.table(df)
 
 st.write("***ก่อนตลาดเปิดตรวจสอบ TB ล่าสุด > RE เมื่อตลอดเปิด")
         # st.stop()
