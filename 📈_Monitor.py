a505c55d906efaaab131cfe4371fc5f551c1c032
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2023-12-28 22:10:51 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -41,7 +41,7 @@ tickerData['action'] = data
 tickerData['index'] = [ i+1 for i in range(len(tickerData))]
 
 tickerData_1 = pd.DataFrame(columns=(tickerData.columns))
-tickerData_1['action'] = np.random.randint(2, size = len(tickerData)+6)[-5:]
+tickerData_1['action'] = 0
 tickerData_1.index = ['+0' , "+1" , "+2" , "+3" , "+4"]
 df = pd.concat([tickerData , tickerData_1], axis=0).fillna("")
 np.random.seed(fx_js)
