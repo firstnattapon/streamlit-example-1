49456398e2dbd6c8f446369087d4e72edc64c2d7
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-01-03 16:17:52 +0700

    Update 1_🔥_Add_CF.py

--- pages/1_🔥_Add_CF.py
+++ pages/1_🔥_Add_CF.py
@@ -9,7 +9,7 @@ channel_id = 2329127
 write_api_key = 'V10DE0HKR4JKB014'
 client = thingspeak.Channel(channel_id, write_api_key)
 
-def NEGG(entry = 1.26 ,ref = 1.26  ):
+def NEGG(entry = 1.26 , ref = 1.26):
     try:
         entry  = entry ; step = 0.01 ;  Fixed_Asset_Value = 1500. ; Cash_Balan = 650.
         if entry < 10000 :
@@ -63,8 +63,7 @@ y_1 = st.number_input('portfolio_cash', step=0.01 )
 y_2 = st.number_input('portfolio_asset', step=0.01 )
 z_1 = st.number_input('Adjust', step=0.01)
 
-st.write("ราคา_NEGG_1", NEGG(ref = x_1 ) ) 
-
+st.write("ราคา_NEGG_1", NEGG(ref = x_1 )) 
 
 
 Check_ADD = st.checkbox('ADD_CF ')
