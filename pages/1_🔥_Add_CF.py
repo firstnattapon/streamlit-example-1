b5a90f12e949c72674908a1152c8c4cef5172f56
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-01-03 16:18:49 +0700

    Update 1_🔥_Add_CF.py

--- pages/1_🔥_Add_CF.py
+++ pages/1_🔥_Add_CF.py
@@ -62,8 +62,8 @@ x_2 = st.number_input('ราคา_FFWM_6.88', step=0.01 )
 y_1 = st.number_input('portfolio_cash', step=0.01 )
 y_2 = st.number_input('portfolio_asset', step=0.01 )
 z_1 = st.number_input('Adjust', step=0.01)
-
-st.write("ราคา_NEGG_1", NEGG(ref = x_1 )) 
+q_1 = NEGG(ref = x_1 )
+st.write("ราคา_NEGG_1.26", q_1 )) 
 
 
 Check_ADD = st.checkbox('ADD_CF ')
