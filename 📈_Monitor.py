86ced98d3470011e479a96f2a39b68b7ca29e397
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-01-03 14:14:19 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -85,7 +85,7 @@ df = pd.concat([tickerData , tickerData_1], axis=0).fillna("")
 np.random.seed(fx_js)
 df['action'] = np.random.randint(2, size = len(df))
 st.write("f(x) {}".format(fx_js))
-st.write("Production {}".format( round(Production('FFWM')),2))
+st.write("Production {}".format( round(Production('FFWM')), 3))
 st.table(df.tail(7))
 
 st.write("***ก่อนตลาดเปิดตรวจสอบ TB ล่าสุด > RE เมื่อตลอดเปิด")
