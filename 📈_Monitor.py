b00b79e6cb3cc36cb4719912fdf5ac9f93d856f3
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-01-07 17:09:35 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -29,7 +29,7 @@ def average_cf (Ticker = 'FFWM' , field = 1 ):
     return tickerData / fx_js_2
 
 cf_day = average_cf()
-st.write( 'average_cf_day:' ,  cf_day , 'average_cf_mo:' , cf_day*30  )
+st.write( 'average_cf_day:' ,  round(cf_day , 2 ) , 'average_cf_mo:' , round(cf_day*30 , 3)  )
     
 
 def Production(Ticker = "FFWM" ):
