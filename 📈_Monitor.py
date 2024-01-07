491f0aa3ffd0e8a72c16a69f93c432a9a14c82c1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-01-07 16:33:57 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -18,7 +18,7 @@ client = thingspeak.Channel(channel_id, write_api_key , fmt='json')
 client_2 = thingspeak.Channel(2394198 , 'OVZNYQBL57GJW5JF' , fmt='json')
 fx_2 = client_2.get_field_last(field='{}'.format(1))
 fx_js_2 = json.loads(fx_2)["field1"]
-st.write(json.loads(fx_2)["field1"])
+st.write(json.loads(fx_2) )
 
 def Production(Ticker = "FFWM" ):
     try:
