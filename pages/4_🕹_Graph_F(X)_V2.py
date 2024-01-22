b07321f8e2e8595ea3a306407a2f234b3fbd6fd3
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-01-22 19:06:26 +0700

    Update 4_🕹_Graph_F(X)_V2.py

--- pages/4_🕹_Graph_F(X)_V2.py
+++ pages/4_🕹_Graph_F(X)_V2.py
@@ -149,8 +149,7 @@ def delta_x (Ticker = 'FFWM' , number = [36 , 68]):
         Mean_min = (chart_data[['min']].values)
         Mean_max = (chart_data[['max']].values)
         Mean_a = Mean_min + (( Mean_max - Mean_min) / 2 )
-        # chart_data['Mean'] =  chart_data[['min']] + ((chart_data[['max']] - chart_data[['min']]) / 2 )
-        st.write(Mean_a) 
+        chart_data['Mean'] = Mean_a
         st.line_chart(chart_data)
         st.stop()
 
