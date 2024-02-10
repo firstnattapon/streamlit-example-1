9effc347a20459db623659b83956a14a338dad84
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-10 19:18:36 +0700

    Update and rename 5_🕹_EX_F(X).py to 5_🕹_Exist_F(X).py

--- pages/5_🕹_Exist_F(X).py
+++ pages/5_🕹_Exist_F(X).py
@@ -4,4 +4,4 @@ import yfinance as yf
 import streamlit as st
 import thingspeak
 import json
-st.set_page_config(page_title="Graph_F(X)", page_icon="🕹")
+st.set_page_config(page_title="Exist_F(X)", page_icon="🕹")
