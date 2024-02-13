c7208b5132dc314a3ea7a383d1a66ad2bdfe1a23
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-13 14:50:19 +0700

    Update 5_☀_Exist_F(X).py

--- pages/5_☀_Exist_F(X).py
+++ pages/5_☀_Exist_F(X).py
@@ -166,8 +166,8 @@ max_dd = pd.DataFrame( pro  ,  columns= columns )
 max_dd['FFWM'] =   [ (i - np.max(max_dd.FFWM.values))    for i in max_dd.FFWM.values]
 max_dd['NEGG'] =   [ (i - np.max(max_dd.NEGG.values))    for i in max_dd.NEGG.values]
 
-line_1 =    max_dd.FFWM.values  / abs(np.min(max_dd.FFWM.values))   
-line_2 =   max_dd.NEGG.values  / abs(np.min(max_dd.NEGG.values))   
+line_1 =    max_dd.FFWM.values  / abs(np.max(max_dd.FFWM.values))   
+line_2 =   max_dd.NEGG.values  / abs(np.max(max_dd.NEGG.values))   
 line_3 =   np.array([line_1 , line_2]).T
 
 st.write('Production vs Delta')
