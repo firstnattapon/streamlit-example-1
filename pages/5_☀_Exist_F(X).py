e56f49d711dfd9f585e1b5a0364be005d2d1183f
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-13 15:11:15 +0700

    Update 5_☀_Exist_F(X).py

--- pages/5_☀_Exist_F(X).py
+++ pages/5_☀_Exist_F(X).py
@@ -166,15 +166,14 @@ max_dd = pd.DataFrame( pro  ,  columns= columns )
 max_dd['FFWM'] =   [ (i - np.max(max_dd.FFWM.values))    for i in max_dd.FFWM.values]
 max_dd['NEGG'] =   [ (i - np.max(max_dd.NEGG.values))    for i in max_dd.NEGG.values]
 
-# line_1  =    abs(np.max(max_dd.FFWM.values))   
-line_2   =    all_Pro_1 / abs( np.min(max_dd.NEGG.values))  
-# line_3 =   np.array([line_1 , line_2]).T
-st.write(line_2)
+line_1  =    abs( np.min(max_dd.FFWM.values)) / all_Pro_1 
+line_2   =   abs( np.min(max_dd.NEGG.values)) / all_Pro_2  
+line_3 =    np.array([line_1 , line_2]).T
 
 st.write('Production vs Delta')
 st.line_chart(pd.DataFrame( line*100  ,  columns= columns ))
 st.write('Buffer vs Delta')
-# st.line_chart(pd.DataFrame( line_3  ,  columns= columns ))
+st.line_chart(pd.DataFrame( line_3  ,  columns= columns ))
 st.write('Production')
 st.line_chart(pd.DataFrame( pro  ,  columns= columns ))
 st.write('Buffer')
