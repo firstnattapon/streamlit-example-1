645b4d9638280e9c13db638dbca847bae136d21b
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-13 13:53:25 +0700

    Update 5_☀_Exist_F(X).py

--- pages/5_☀_Exist_F(X).py
+++ pages/5_☀_Exist_F(X).py
@@ -165,9 +165,9 @@ line =   np.array([all_fx_1 , all_fx_2]).T
 columns =  np.array([Ticker_1 , Ticker_2])
 pro =   np.array([all_Pro_1 , all_Pro_2]).T
 max_dd = pd.DataFrame( pro  ,  columns= columns )
-dd_1 = np.max(max_dd.FFWM.values)
+
 max_dd['FFWM'] =   [np.max(max_dd.FFWM.values) - i   for i in max_dd.FFWM.values]
-max_dd['FFWM'] =   [ np.max(max_dd.NEGG.values) - i   for i in max_dd.NEGG.values]
+max_dd['NEGG'] =   [ np.max(max_dd.NEGG.values) - i   for i in max_dd.NEGG.values]
 
 st.write('Production vs Delta')
 st.line_chart(pd.DataFrame( line*100  ,  columns= columns ))
