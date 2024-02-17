ecf69709ada5b57afac36eb77ac7b5ce86fba5a6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:43:19 +0700

    Update 7_📕_Principle_F(X).py

--- pages/7_📕_Principle_F(X).py
+++ pages/7_📕_Principle_F(X).py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Exist_F(X)", page_icon="📕")
+st.set_page_config(page_title="Principle_F(X)", page_icon="📕")
 
 # st.set_page_config(layout="wide")
 
