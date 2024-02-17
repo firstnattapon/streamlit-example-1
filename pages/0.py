6f6d51c5a5825b39a30564d112a0a0916eb1a457
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 20:29:17 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -8,11 +8,5 @@ import yfinance as yf
 st.set_page_config(page_title="0", page_icon="🔥")
 
 
-# Define the MindMeister map URL
 map_url = "https://www.mindmeister.com/app/map/3066443605?t=dxos6u4HQQ&m=outline"
-
-# Create a heading for your app
-st.title("MindMeister Map Viewer")
-
-# Use the `iframe` function to display the map
 st.iframe(map_url)
