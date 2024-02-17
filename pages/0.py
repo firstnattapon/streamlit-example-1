7e537eb248fdaa79e87341745d104dea0dcf226f
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:06:59 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -2,8 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-components.iframe(
-        f"https://www.google.com/search?igu=1&ei=&q={search}", height=1000, width=400
-    ) 
-
-
+<div><iframe width="600" height="400" frameBorder="0" src="https://www.mindmeister.com/maps/public_map_shell/3066443605/principle-by-first?width=600&height=400&z=auto&no_share=1&no_logo=1" scrolling="no" style="overflow:hidden;margin-bottom:5px">Your browser is not able to display frames. Please visit <a href="https://www.mindmeister.com/3066443605/principle-by-first" target="_blank">Principle (หลักการ) by first</a> on MindMeister.</iframe></div>
