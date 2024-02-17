e15d40af601b5c722333cb45384f0ab0761430b5
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:00:46 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import numpy as np
-import datetime
-import thingspeak
-import pandas as pd
-import yfinance as yf
-
 import streamlit.components.v1 as components
-components.iframe("http://www.soccersuck.com/")
 
+
+components.iframe("<div><iframe width="600" height="400" frameBorder="0" src="https://www.mindmeister.com/maps/public_map_shell/3066443605/principle-by-first?width=600&height=400&z=auto&no_share=1&no_logo=1" scrolling="no" style="overflow:hidden;margin-bottom:5px">Your browser is not able to display frames. Please visit <a href="https://www.mindmeister.com/3066443605/principle-by-first" target="_blank">Principle (หลักการ) by first</a> on MindMeister.</iframe></div>")
  
