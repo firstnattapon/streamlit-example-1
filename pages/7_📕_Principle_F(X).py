6aa7b3cff2a752c45d24018c46a6c166534d0571
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:44:24 +0700

    Update 7_📕_Principle_F(X).py

--- pages/7_📕_Principle_F(X).py
+++ pages/7_📕_Principle_F(X).py
@@ -2,11 +2,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Principle_F(X)", page_icon="📕" , layout="wide")
-# st.set_page_config(layout="wide")
 
-# @st.cache_data
-# def iframe ():
-#   src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
-#   st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
+@st.cache_data
+def iframe ():
+  src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
+  st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
   
-# iframe()
+iframe()
