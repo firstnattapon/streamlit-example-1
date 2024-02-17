962dfff224fd4e0da962ccb5c4a1e3557939ea4e
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 20:28:50 +0700

    Update and rename 0 to 0.py

--- pages/0.py
+++ pages/0.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import numpy as np
+import datetime
+import thingspeak
+import pandas as pd
+import yfinance as yf
+
+st.set_page_config(page_title="0", page_icon="🔥")
+
 
 # Define the MindMeister map URL
 map_url = "https://www.mindmeister.com/app/map/3066443605?t=dxos6u4HQQ&m=outline"
