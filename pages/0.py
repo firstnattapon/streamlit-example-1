f70a62031f4a158e311f596ea78a864ac49f0ae5
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:23:01 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 
 src="https://www.mindmeister.com/maps/public_map_shell/3066443605/principle-by-first?width=600&height=1000&z=auto&no_share=1&no_logo=1" 
-
+src= "https://www.mindmeister.com/app/map/3066443605?m=outline"
 
 st.components.v1.iframe(src, width=600, height=1000, scrolling=False)
 
