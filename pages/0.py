f1cb5ac700a131eaa854279b913d0d12975201ff
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:02:40 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-components.iframe("https://www.mindmeister.com/3066443605/principle-by-first" target="_blank")
+components.iframe("https://www.mindmeister.com/3066443605/principle-by-first")
  
 
 
