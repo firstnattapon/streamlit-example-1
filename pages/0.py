4884220969abdbfec082c6a25482aa2dd3b27d05
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:13:55 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-components.iframe()
+components.iframe("https://www.mindmeister.com/maps/public_map_shell/3066443605/principle-by-first?width=600&height=400&z=auto&no_share=1&no_logo=1")
