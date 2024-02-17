4b7d017b91af5018558ccd665f76a67d31509649
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:32:39 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.set_page_config(layout="wide")
 
-src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
 
-st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
-
+def iframe ():
+  src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
+  st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
+  
+iframe()
