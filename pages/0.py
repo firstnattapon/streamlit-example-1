0264b27c91937c7794a11e0f83f3147544652b9b
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:24:48 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-src="https://www.mindmeister.com/maps/public_map_shell/3066443605/principle-by-first?width=600&height=1000&z=auto&no_share=1&no_logo=1" 
+src="https://www.mindmeister.com/maps/public_map_shell/3066443605/principle-by-first?width=600&height=2000&z=auto&no_share=1&no_logo=1" 
 
 st.components.v1.iframe(src, width=1000, height=1000, scrolling=False)
 
