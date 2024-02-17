0c40a212b72a2c1260fb8ab7fd6122b40fdc928e
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 22:55:33 +0700

    Update 7_📕_Principle_F(X).py

--- pages/7_📕_Principle_F(X).py
+++ pages/7_📕_Principle_F(X).py
@@ -7,5 +7,8 @@ st.set_page_config(page_title="Principle_F(X)", page_icon="📕" , layout="wide"
 def iframe ():
   src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
   st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
-  
+
+st.write('____')
 iframe()
+st.write('____')
+st.write(https://www.mindmeister.com/app/map/3066443605?m=outline&t=XZPVgoJ9jm)
