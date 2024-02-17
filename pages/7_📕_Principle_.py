8164f0d5833afc90a61882c18f82f6d46a07c950
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:42:12 +0700

    Update 7_📕_Principle_.py

--- pages/7_📕_Principle_.py
+++ pages/7_📕_Principle_.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.set_page_config(layout="wide")
 
-@st.cache_data
-def iframe ():
-  src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
-  st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
+st.set_page_config(page_title="Exist_F(X)", page_icon="☀")
+
+# st.set_page_config(layout="wide")
+
+# @st.cache_data
+# def iframe ():
+#   src="https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline" 
+#   st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
   
-iframe()
+# iframe()
