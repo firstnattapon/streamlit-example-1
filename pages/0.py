9574f6ec6b1bf8fb2014f36693e35933dfef5825
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-17 21:18:59 +0700

    Update 0.py

--- pages/0.py
+++ pages/0.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-src = "https://www.mindmeister.com/app/map/3066443605?fullscreen=1&v=embedded&m=outline"
+src = "https://www.mindmeister.com/3066443605/principle-by-first"
 
 st.components.v1.iframe(src, width=600, height=1000, scrolling=False)
 
