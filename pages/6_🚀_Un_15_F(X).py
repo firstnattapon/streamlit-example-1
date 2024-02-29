f943ff620b771927741db711466ceb8f2288c3c2
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-02-29 14:30:51 +0700

    Update 6_🚀_Un_15_F(X).py

--- pages/6_🚀_Un_15_F(X).py
+++ pages/6_🚀_Un_15_F(X).py
@@ -148,10 +148,10 @@ if checkbox3 :
 checkbox1 = st.checkbox(' Sum_Delta / Max.Sum_Buffer %' , value=0 )
 if checkbox1 :
     Delta_2 = Delta
-    Delta_2['FFWM_Delta/FFWM.max_Buffer'] =  (Delta.FFWM_net_pv.values  /  abs(np.min(Buffer.FFWM_Buffer.values))) *100
-    Delta_2['NEGG_Delta/NEGG.max_Buffer'] =  (Delta.NEGG_net_pv.values  /  abs(np.min(Buffer.NEGG_Buffer.values))) *100
-    Delta_2['Sum_Delta/Max.Sum_Buffer%'] = (Delta.Sum_Delta.values / abs( np.min(Sum_Buffer.Sum_Buffer.values))) *100
-    Delta_2 = Delta_2[['FFWM_Delta/FFWM.max_Buffer' , 'NEGG_Delta/NEGG.max_Buffer' , 'Sum_Delta/Max.Sum_Buffer%'  ]]
+    Delta_2['FFWM'] =  (Delta.FFWM_net_pv.values  /  abs(np.min(Buffer.FFWM_Buffer.values))) *100
+    Delta_2['NEGG'] =  (Delta.NEGG_net_pv.values  /  abs(np.min(Buffer.NEGG_Buffer.values))) *100
+    Delta_2['Sum_Delta / Max.Sum_Buffer %'] = (Delta.Sum_Delta.values / abs( np.min(Sum_Buffer.Sum_Buffer.values))) *100
+    Delta_2 = Delta_2[['FFWM' , 'NEGG' , 'Sum_Delta / Max.Sum_Buffer %'  ]]
     st.line_chart(Delta_2)
 
 
