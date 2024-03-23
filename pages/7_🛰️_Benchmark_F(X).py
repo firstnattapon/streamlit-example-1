b52f748bf0e2be7cb2dc765ac7357edd79a09604
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-23 22:25:59 +0700

    Update 7_🛰️_Benchmark_F(X).py

--- pages/7_🛰️_Benchmark_F(X).py
+++ pages/7_🛰️_Benchmark_F(X).py
@@ -90,8 +90,9 @@ def delta2(Ticker = "FFWM" , pred = 1 ,  filter_date = '2022-12-21 12:00:00+07:0
             tickerData['refer_model'] = refer_model
             tickerData['pv'] =  tickerData['Cash_Balan'] + ( tickerData['Amount_Asset'] * tickerData['Close']  )
             tickerData['refer_pv'] = tickerData['refer_model'] + Fixed_Asset_Value
-            tickerData['net_pv'] =   tickerData['pv'] - tickerData['refer_pv']  
-            final = tickerData[['net_pv' , 'Close']]
+            tickerData['net_pv'] =   tickerData['pv'] - tickerData['refer_pv']
+            tickerData = tickerData.reset_index()
+            final = tickerData[['re' , 'net_pv']]
             return  final
     except:pass
 
