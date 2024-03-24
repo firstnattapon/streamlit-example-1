342934c3dc53d06a363f0cee351e8320637ea1c0
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-24 23:17:38 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -170,6 +170,7 @@ try:
         Delta_2 = Delta_2[[ 'S&P_500_ETF' , 'NASDAQ_100_ETF' , '{}'.format(Ticker_s[2]) ]]
         st.line_chart(Delta_2)
 
+        st.line_chart(Sum_Buffer[{}_re.format(title)])
         st.write(Sum_Buffer)
 
 except:pass
