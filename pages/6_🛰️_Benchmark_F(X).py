fdbb10402b0987f60dc80f0e6854ceab7d115525
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-24 23:44:09 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -170,7 +170,6 @@ try:
         Delta_2 = Delta_2[[ 'S&P_500_ETF' , 'NASDAQ_100_ETF' , '{}'.format(Ticker_s[2]) ]]
         st.line_chart(Delta_2)
         st.line_chart(Delta['{}_net_pv'.format(title)])
-        st.scatter_chart(Sum_Buffer['{}_re'.format(title)] ,  )
 
 
         tickerData = yf.Ticker(title)
@@ -181,6 +180,7 @@ try:
         filter_date_2 = '2022-12-21 12:00:00+07:00'
         tickerData_2 = tickerData[tickerData.index >= filter_date_2]
         st.line_chart(tickerData_2.values)
+        st.scatter_chart(Sum_Buffer['{}_re'.format(title)] ,  )
         st.line_chart(tickerData_1.values)
 
 
