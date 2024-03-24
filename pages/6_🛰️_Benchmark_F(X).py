fdb02eedb14a03a21edf2caf2c1d49f1054cdfd8
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-24 23:12:01 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -179,7 +179,7 @@ filter_date_1 = '2020-12-21 12:00:00+07:00'
 tickerData_1 = tickerData[tickerData.index >= filter_date_1]
 filter_date_2 = '2022-12-21 12:00:00+07:00'
 tickerData_2 = tickerData[tickerData.index >= filter_date_2]
-st.line_chart(tickerData_2.values)
+st.scatter_chart(tickerData_2.values)
 st.scatter_chart(tickerData_1.values)
 
 st.write('https://www.mindmeister.com/app/map/3178532454?m=outline')
