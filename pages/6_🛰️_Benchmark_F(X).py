89a1eccdf2756497d944f5e42ca200c697c09e36
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-25 15:59:22 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -183,9 +183,9 @@ if checkbox1 :
     tickerData_2 = tickerData[tickerData.index >= filter_date_2]
 
     st.line_chart(Delta_2)
-    tickerData_2['diff'] = diff_fx
+    tickerData_2['diff_fx'] = diff_fx
     st.write(tickerData_2)
-    # st.scatter_chart(diff , x='c', size='diff_fx')
+    st.scatter_chart(tickerData_2 , x='Close', size='diff_fx')
     
     # st.scatter_chart( diff     )
     st.line_chart(Delta['{}_net_pv'.format(title)])
