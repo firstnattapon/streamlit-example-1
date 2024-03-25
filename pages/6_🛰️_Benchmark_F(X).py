4b1c0cea46bb61bccf3e39896c60c4d6fc214085
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-25 19:33:21 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -188,7 +188,8 @@ if checkbox1 :
     fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram" , text_auto=True)
     st.plotly_chart(fig, use_container_width=True)
     st.line_chart(Delta['{}_net_pv'.format(title)])
-    st.scatter_chart( tickerData_2  , size= 'Diff' )
+    # st.scatter_chart( tickerData_2  , size= 'Diff' )
+    st.line_chart(tickerData_2.values)
     st.line_chart(tickerData_1.values)
     st.line_chart(Delta_2)
 
