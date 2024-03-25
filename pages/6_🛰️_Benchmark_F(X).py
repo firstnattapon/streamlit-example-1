27b94448bffe3da9a5c6e32c003cf3900c4caff9
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-25 19:39:22 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -185,11 +185,11 @@ if checkbox1 :
     tickerData_2 = tickerData[tickerData.index >= filter_date_2]
     tickerData_2['Diff'] = diff_fx
 
-    fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram" , text_auto=True)
+    fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram" , text_auto=True , color_continuous_scale = 'gnbu')
     st.plotly_chart(fig, use_container_width=True)
     st.line_chart(Delta['{}_net_pv'.format(title)])
     # st.scatter_chart( tickerData_2  , size= 'Diff' )
-    st.line_chart(tickerData_2.values)
+    st.line_chart(tickerData_2.Close.values)
     st.line_chart(tickerData_1.values)
     st.line_chart(Delta_2)
 
