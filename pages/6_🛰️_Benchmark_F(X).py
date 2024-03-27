dd241610bec0d9faa322643251d12ab537568eae
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 18:20:42 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -202,9 +202,9 @@ if checkbox1 :
     # Delta_2['Gold_ETF'] = Delta_2.add_risk.values  /                Delta_2['{}_net_pv'.format(Ticker_s[2])].values 
     # Delta_2['{}'.format(Ticker_s[3])] =  Delta_2.add_risk.values /  Delta_2['{}_net_pv'.format(Ticker_s[3])].values 
 
-    Delta_2['S&P_500_ETF'] = 0
-    Delta_2['NASDAQ_100_ETF'] = 0
-    Delta_2['Gold_ETF'] = 0
+    Delta_2['S&P_500_ETF'] = Delta_2['{}_net_pv'.format(Ticker_s[0])].values/(1500+(abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[0])].values))+abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[0])].values))))*100
+    Delta_2['NASDAQ_100_ETF'] = Delta_2['{}_net_pv'.format(Ticker_s[1])].values/(1500+(abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[1])].values))+abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[1])].values))))*100
+    Delta_2['Gold_ETF'] = Delta_2['{}_net_pv'.format(Ticker_s[2])].values/(1500+(abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[2])].values))+abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[2])].values))))*100
     Delta_2['{}'.format(Ticker_s[3])]=Delta_2['{}_net_pv'.format(Ticker_s[3])].values/(1500+(abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values))+abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values))))*100
 
     Delta_2 = Delta_2[[ 'S&P_500_ETF' , 'NASDAQ_100_ETF' , 'Gold_ETF' , '{}'.format(Ticker_s[3]) ]]
