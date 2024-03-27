4da28acda0b7eed993f8c75f1397d67388df8a34
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 21:23:03 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -220,8 +220,9 @@ if checkbox1 :
 
     number = st.number_input('nbinsy', value=50 , step =1 , min_value=1  ) 
     color = st.text_input('color', 'viridis')
+    st.write(px.colors.sequential.Viridis)
     
-    fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram"  , text_auto=True , nbinsy=number , color_continuous_scale = color)
+    fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram"  , text_auto=True , nbinsy=number , color_continuous_scale = color )
     fig.add_shape(type='line', x0=-1 , y0=tickerData_2.Close.values[-1] , x1= max(tickerData_2.Diff)   , y1= tickerData_2.Close.values[-1], line=dict(color='Red')  )
     st.plotly_chart(fig, use_container_width=True)
     st.write('Accumulation & Distribution' ,'vs' , 'Emotional_Marketing' )
