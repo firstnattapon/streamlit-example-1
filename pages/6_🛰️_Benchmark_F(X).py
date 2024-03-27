b13b677878c9065a9d8d7e0f61fe459713f9202e
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 13:49:19 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -225,7 +225,7 @@ if checkbox1 :
 
     st.line_chart(Delta_2)
     # st.write('Delta_Adj.Risk' ,  Delta2[{}.format(title)].values[-1] )
-    st.write( Delta_2['{}'.format(Ticker_s[3])])
+    st.write( Delta_2['{}'.format(Ticker_s[3])].values[-1] )
 
     st.line_chart(tickerData_2.Close.values)
     st.line_chart(tickerData_1.values)
