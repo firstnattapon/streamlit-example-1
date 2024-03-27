9ad6bc7dc4708a13395c2abdbd60ccf46be10c65
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 20:40:34 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -217,7 +217,7 @@ if checkbox1 :
     survival =  (abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) +  abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) )  
     st.write('____')
     st.write('Data' , add_risk , 1500 / add_risk ,  (1500+survival) / add_risk  , '< Premium & Discount'  )
-    fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram"  , text_auto=True , facet_col_spacing  = 0.10 )
+    fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram"  , text_auto=True , range_x   = 0.10 )
     fig.add_shape(type='line', x0=-1 , y0=tickerData_2.Close.values[-1] , x1= max(tickerData_2.Diff)   , y1= tickerData_2.Close.values[-1], line=dict(color='Red'))
     st.plotly_chart(fig, use_container_width=True)
     st.write('Accumulation & Distribution' ,'vs' , 'Emotional_Marketing' )
