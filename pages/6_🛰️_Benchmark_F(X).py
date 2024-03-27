fca8848d58fa45cdbbad46e5f9f8388865c3f3f7
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 16:02:25 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -223,7 +223,7 @@ if checkbox1 :
     st.line_chart(Delta['{}_net_pv'.format(title)] )
     add_risk =  Delta['{}_net_pv'.format(title)].values[-1]
     bf = (abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) +  abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values))) 
-    st.write('Delta_Add.Risk' , add_risk , 1500 / add_risk ,    1500+bf  / add_risk  )
+    st.write('Delta_Add.Risk' , add_risk , 1500 / add_risk ,    1500 + bf[-1]  / add_risk  )
 
     st.line_chart(Delta_2)
     Adj_Risk = Delta_2['{}'.format(Ticker_s[3])].values[-1]
