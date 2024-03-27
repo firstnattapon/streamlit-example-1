7b5f22377296728dac06ecf596a368b161cd701a
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 19:57:22 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -216,7 +216,7 @@ if checkbox1 :
     add_risk =  Delta['{}_net_pv'.format(title)].values[-1]
     survival =  (abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) +  abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) )  
     st.write('____')
-    st.write('Data' , add_risk , 1500 / add_risk , (1500+survival) / add_risk )
+    st.write('Data' , add_risk , 1500 / add_risk , '  (1500+survival) / add_risk  , '< Premium กับ Discount'  )
     fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram"  , text_auto=True )
     fig.add_shape(type='line', x0=-1 , y0=tickerData_2.Close.values[-1] , x1= max(tickerData_2.Diff)   , y1= tickerData_2.Close.values[-1], line=dict(color='Red'))
     st.plotly_chart(fig, use_container_width=True)
