427953510eb774a70e9b2cd39bb3b7e066bb11cb
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 18:58:48 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -214,17 +214,18 @@ if checkbox1 :
     tickerData_2 = tickerData[tickerData.index >= filter_date_2]
     tickerData_2['Diff'] = diff_fx
 
+
+    add_risk =  Delta['{}_net_pv'.format(title)].values[-1]
+    survival =  (abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) +  abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) )   
+    st.write('Delta_Add.Risk' , add_risk , 1500 / add_risk , (1500+survival) / add_risk )
+    
     fig = px.density_heatmap(tickerData_2 , x="Diff", y="Close",   marginal_y="histogram"  , text_auto=True )
     fig.add_shape(type='line', x0=-1 , y0=tickerData_2.Close.values[-1] , x1= max(tickerData_2.Diff)   , y1= tickerData_2.Close.values[-1], line=dict(color='Red'))
     st.plotly_chart(fig, use_container_width=True)
 
 
-    add_risk =  Delta['{}_net_pv'.format(title)].values[-1]
-    survival =  (abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) +  abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) )   
-    
-    st.line_chart(Delta['{}_net_pv'.format(title)] )
-    st.write('Delta_Add.Risk' , add_risk , 1500 / add_risk , (1500+survival) / add_risk )
 
+    st.line_chart(Delta['{}_net_pv'.format(title)] )
     st.line_chart(Delta_2)
     
     st.line_chart(tickerData_2.Close.values)
