7c18a303371c59932e4a8d4e02b65982aa7a4e70
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-27 17:19:03 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -201,12 +201,13 @@ if checkbox1 :
     add_risk_0 =   Delta['{}_net_pv'.format(Ticker_s[3])].values 
     survival_0 =  (abs(np.min( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) +  abs(np.max( Buffer['{}_Buffer'.format(Ticker_s[3])].values)) )
     
-    Delta_2['S&P_500_ETF'] =  0
-    Delta_2['NASDAQ_100_ETF'] = 0 
-    Delta_2['Gold_ETF'] =  0 
+    # Delta_2['S&P_500_ETF'] =  0
+    # Delta_2['NASDAQ_100_ETF'] = 0 
+    # Delta_2['Gold_ETF'] =  0 
     Delta_2['{}'.format(Ticker_s[3])] =  (1500+survival_0) / add_risk_0 
     
     Delta_2 = Delta_2[[ 'S&P_500_ETF' , 'NASDAQ_100_ETF' , 'Gold_ETF' , '{}'.format(Ticker_s[3]) ]]
+    Delta_2 = Delta_2[[  '{}'.format(Ticker_s[3]) ]]
 
     tickerData = yf.Ticker(title)
     tickerData = tickerData.history(period= 'max' )[['Close']]
