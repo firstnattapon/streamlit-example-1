df9b95f98fbe3d3354c4de716cf45c830934fe43
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-03-29 18:45:48 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -236,8 +236,6 @@ if checkbox1 :
     fig_2 = px.line(tickerData_2 , y='Close' )
     fig_3 = px.line( Delta['{}_net_pv'.format(title)] , y='{}_net_pv'.format(title))
 
-    fig_2.add_line( Delta )
- 
     col3.plotly_chart( fig_2  , use_container_width=True)
     col4.plotly_chart(fig, use_container_width=True)
     col5.plotly_chart(fig_3, use_container_width=True)
