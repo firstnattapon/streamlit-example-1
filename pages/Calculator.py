b61658c8ad850feafaa4239cad3c7fd89ac74caa
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-04-23 19:29:01 +0700

    Update Calculator.py

--- pages/Calculator.py
+++ pages/Calculator.py
@@ -31,13 +31,11 @@ x_3 = st.number_input('NEGG_ASSET', step=0.001 , value=0.01 )
 x_4 = st.number_input('FFWM_ASSET', step=0.001  , value=0.01  )
 st.write("_____") 
 
-col1, col2  = st.columns(3)
 
-col1.write("Limut_order_Sell_NEGG") 
+st.write("Limut_order_Sell_NEGG") 
 s8 , s9 , s10 =  sell(x_3 , Diff= x_2)
-col2.write('P' , s8 ,'A', s9 , 'C' ,s10 )
+st.write('NEGG>             ' ,'P' , s8 ,'A', s9 , 'C' ,s10 )
 
-# col3, col4  = st.columns(2)
 
 st.write("Limut_order_Sell_FFWM") 
 s11 , s12 , s13 =  sell(x_4 , Diff= x_2)
