dcbaaf1bba4fbd65fed50d8fa507b99d302c1c36
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-04-23 19:34:02 +0700

    Update Calculator.py

--- pages/Calculator.py
+++ pages/Calculator.py
@@ -20,7 +20,16 @@ def sell (x_3 , fix_c=1500 , Diff=60):
   s6 =  s5*s2
   s7 =  (x_3 * s2) + s6
   return s2 , s5 , round(s7, 2)
-  
+
+def buy (x_3 , fix_c=1500 , Diff=60):
+  b1 =  (1500+Diff) /x_3
+  b2 =  round(b1, 2)
+  b3 =  b2*x_3
+  b4 =  abs(b3 - fix_c)
+  b5 =  round( b4 / b2 )  
+  b6 =  b5*b2
+  b7 =  (x_3 * b2) + b6
+  return b2 , b5 , round(b7, 2)
   
 x_2 = st.number_input('Diff', step=1 , value= 60  )
 st.write("_____") 
@@ -39,7 +48,11 @@ s11 , s12 , s13 =  sell(x_4 , Diff= x_2)
 st.write('P' , s11 ,'A', s12 , 'C' ,s13 )
 st.write("_____") 
 
-  
+st.write("Limut Order_Buy_NEGG") 
+b8 , b9 , b10 =  buy(x_3 , Diff= x_2)
+st.write('P' , b8 ,'A', b9 , 'C' ,b10 )
+
+
 st.write("""เป้าหมาย / asset
 ปัดลงหาราคา 
 ได้ราคาคูณของ
