56347a224bfa224542aec2f083fea0d2552c090a
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-04-23 17:42:26 +0700

    Update and rename 000 to Calculator.py

---
+++ pages/Calculator.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import datetime
+import thingspeak
+import pandas as pd
+import yfinance as yf
+
+st.set_page_config(page_title="Calculator", page_icon="🔥")
+
+x_1 = st.number_input('ราคา_NEGG_1.26' , step=0.01 , value =  yf.Ticker('NEGG').fast_info['lastPrice']   )
+x_2 = st.number_input('ราคา_FFWM_6.88', step=0.01 ,   value = yf.Ticker('FFWM').fast_info['lastPrice']   )
+st.write("_____") 
