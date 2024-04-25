05a2ae17088929d5a274004765bdac061b6963b0
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-04-25 14:17:44 +0700

    Update Calculator.py

--- pages/Calculator.py
+++ pages/Calculator.py
@@ -36,20 +36,20 @@ st.write("_____")
 
 try:
   s8 , s9 , s10 =  sell(x_3 , Diff= x_2)
-  st.write("Limut Order_Buy_NEGG") 
-  st.write('A',  s9  ,  'P' , s8 , 'C' ,s10 )
-  
-  st.write("Limut Order_Buy_FFWM") 
   s11 , s12 , s13 =  sell(x_4 , Diff= x_2)
-  st.write(  'A', s12 ,'P' , s11 , 'C' ,s13 )
-  st.write("_____") 
-
   b8 , b9 , b10 =  buy(x_3 , Diff= x_2)
-  st.write("Limut Order_sell_NEGG") 
-  st.write( 'A', b9  , 'P' , b8 ,'C' ,b10 )
-
   b11 , b12 , b13 =  buy(x_4 , Diff= x_2)
-  st.write("Limut Order_sell_FFWM") 
-  st.write( 'A', b12 , 'P' , b11  , 'C' ,b13 )
+
+  st.write("Limut_Order_NEGG") 
+  st.write( 'A', b9  , 'P' , b8 ,'C' ,b10 )
+  st.write(yf.Ticker('NEGG').fast_info['lastPrice'])
+  st.write('A',  s9  ,  'P' , s8 , 'C' ,s10 )
   st.write("_____") 
+  
+  st.write("Limut Order_FFWM") 
+  st.write( 'A', b12 , 'P' , b11  , 'C' ,b13 )
+  st.write(yf.Ticker('FFWM').fast_info['lastPrice'])
+  st.write( 'A', s12 , 'P' , s11  , 'C' ,s13 )
+  st.write("_____") 
+
 except:pass
