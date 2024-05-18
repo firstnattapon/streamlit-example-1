386016d49e9ead80eadbcbfffd34c85f1f291a72
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-05-18 18:22:05 +0700

    Update 4_🕹_Delta_F(X).py

--- pages/4_🕹_Delta_F(X).py
+++ pages/4_🕹_Delta_F(X).py
@@ -218,4 +218,21 @@ if NEGG_Graph_M :
     number = [number_1 , number_2 ]
     delta_x( Ticker = 'NEGG'  , number = number)
 
+#
+st.write("_____") 
+RIVN_Graph = st.checkbox('RIVN_Graph_F(X)')
+if RIVN_Graph :
+    re = st.button("Rerun_Graph")
+    if re :
+        delta_y('RIVN')
+
+RIVN_Graph_M = st.checkbox('RIVN_Graph_F(X)_M')
+if RIVN_Graph_M :
+    number_1  = st.number_input('Insert a number{}'.format(1),step=1 , value=99     ,  key=1 )
+    number_2 =  st.number_input('Insert a number{}'.format(2),step=1 , value=144    , key=2 )
+    all_id_m = [] ; all_m = []
+    number = [number_1 , number_2 ]
+    delta_x( Ticker = 'RIVN'  , number = number)
+st.write("_____") 
+
 st.stop()
