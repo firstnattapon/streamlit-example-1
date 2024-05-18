85ac5ab513d41d140a64d872cf4f98dca90ddc32
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-05-18 19:55:01 +0700

    Update 7_🚀_Un_15_F(X).py

--- pages/7_🚀_Un_15_F(X).py
+++ pages/7_🚀_Un_15_F(X).py
@@ -149,7 +149,7 @@ if checkbox2 :
 checkbox3 = st.checkbox('Buffer $' , value=1 )
 if checkbox3 :
     st.line_chart(Sum_Buffer)
-    st.write( Sum_Buffer['Sum_Buffer'].values )
+    st.write (  abs( np.min(Buffer.Sum_Buffer.values))  )
     st.line_chart(Buffer)
 
 checkbox1 = st.checkbox('Delta / Survival ' , value=1 )
