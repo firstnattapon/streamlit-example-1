7415df3d37d6364bc289dccfb70977fa197aafa0
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-07-21 13:40:29 +0700

    Update 7_🚀_Un_15_F(X).py

--- pages/7_🚀_Un_15_F(X).py
+++ pages/7_🚀_Un_15_F(X).py
@@ -149,7 +149,7 @@ def Un_15 (Ticker = '' , seed = 36 ):
 
 
 title = st.text_input("Ticker",  )
-ticker_list = eval(ticker_text)
+ticker_list = eval(title)
 
 # Delta , Sum_Buffer , Buffer =  Un_15(Ticker = ['FFWM' , 'NEGG' ,'RIVN','APLS'] ,seed = { 'FFWM' :36 , 'NEGG' :553 ,'RIVN':1075 ,'APLS':168 })
 Delta , Sum_Buffer , Buffer =  Un_15(Ticker = ticker_list  ,seed = { 'FFWM' :36 , 'NEGG' :553 ,'RIVN':1075 ,'APLS':168 })
