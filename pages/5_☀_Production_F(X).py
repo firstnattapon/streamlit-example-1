a9f91021ca2aa5d11c6f495086cd9853c77ec3c1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-07-21 13:03:50 +0700

    Update 5_☀_Production_F(X).py

--- pages/5_☀_Production_F(X).py
+++ pages/5_☀_Production_F(X).py
@@ -159,21 +159,24 @@ def exist (Ticker = '' , seed = 36 ):
 Ticker_1 , all_fx_1  , all_Pro_1  , prd_1  =  exist(Ticker = "FFWM" , seed = 36 )
 Ticker_2 , all_fx_2  , all_Pro_2  , prd_2 =  exist(Ticker = "NEGG" ,  seed = 553 )
 Ticker_3 , all_fx_3  , all_Pro_3  , prd_3 =  exist(Ticker = "RIVN" ,  seed = 1075 )
+Ticker_4 , all_fx_4  , all_Pro_4  , prd_4 =  exist(Ticker = "APLS" ,  seed = 168  )
 
-line =   np.array([all_fx_1 , all_fx_2 ,  all_fx_3 ]).T
-columns =  np.array([Ticker_1 , Ticker_2 , Ticker_3 ])
-pro =   np.array([all_Pro_1 , all_Pro_2 , all_Pro_3  ]).T
+line =   np.array([all_fx_1 , all_fx_2 ,  all_fx_3 , all_fx_4 ]).T
+columns =  np.array([Ticker_1 , Ticker_2 , Ticker_3 , Ticker_4 ])
+pro =   np.array([all_Pro_1 , all_Pro_2 , all_Pro_3 , all_Pro_4  ]).T
 
 max_dd = pd.DataFrame( pro  ,  columns= columns )
 max_dd['FFWM'] =   [ (i - np.max(max_dd.FFWM.values))    for i in max_dd.FFWM.values]
 max_dd['NEGG'] =   [ (i - np.max(max_dd.NEGG.values))    for i in max_dd.NEGG.values]
 max_dd['RIVN'] =   [ (i - np.max(max_dd.RIVN.values))    for i in max_dd.RIVN.values]
+max_dd['APLS'] =   [ (i - np.max(max_dd.APLS.values))    for i in max_dd.APLS.values]
 
 line_1  =     prd_1  /  ( abs( np.min(max_dd.FFWM.values)) + 1500 )
 line_2   =    prd_2   /  (abs( np.min(max_dd.NEGG.values)) + 1500 )
 line_3   =    prd_2   /  (abs( np.min(max_dd.RIVN.values)) + 1500 )
+line_4   =    prd_2   /  (abs( np.min(max_dd.APLS.values)) + 1500 )
 
-line_0 =    np.array([line_1 , line_2 , line_3 ]).T
+line_0 =    np.array([line_1 , line_2 , line_3 , line_4 ]).T
 
 
 Production = st.checkbox('Production' , value=1)
