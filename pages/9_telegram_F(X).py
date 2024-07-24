898fde2155d514bb9ce64631fc347f6dba5307d1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-07-24 22:52:55 +0700

    Update 9_telegram_F(X).py

--- pages/9_telegram_F(X).py
+++ pages/9_telegram_F(X).py
@@ -1,16 +1,16 @@
-import telebot
-import streamlit as st
+# import telebot
+# import streamlit as st
 
-TOKEN = '7248188209:AAFhpA9doHJpESvb2BLkIz6sHTmUeUjOG6E'
-chat_id = 7355273754
-bot = telebot.TeleBot(TOKEN)
+# TOKEN = '7248188209:AAFhpA9doHJpESvb2BLkIz6sHTmUeUjOG6E'
+# chat_id = 7355273754
+# bot = telebot.TeleBot(TOKEN)
 
-def send_message(chat_id, text):
-    bot.send_message(chat_id, text)
-
-
-text = 'This is a test message from Python.'
-send_message(chat_id, text)
+# def send_message(chat_id, text):
+#     bot.send_message(chat_id, text)
 
 
-st.write("channel_id = 2528199") 
+# text = 'This is a test message from Python.'
+# send_message(chat_id, text)
+
+
+# st.write("channel_id = 2528199") 
