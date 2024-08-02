9ae4bef3a87f28fee52f28d7722f7ae8885eee9f
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-02 19:15:15 +0700

    Update 9_telegram_F(X).py

--- pages/9_telegram_F(X).py
+++ pages/9_telegram_F(X).py
@@ -57,7 +57,9 @@ def Monitor (Ticker = 'FFWM' , field = 2 ):
     return fx_js
 
 FFWM_fx  = Monitor(Ticker = 'FFWM', field = 2)
+np.random.seed(FFWM_fx)
 st.write(FFWM_fx)
+
 # FFWM_fx , _  = Monitor(Ticker = 'FFWM', field = 2)
 # NEGG_fx , _  = Monitor(Ticker = 'NEGG', field = 3)
 # RIVN_fx , _  = Monitor(Ticker = 'RIVN', field = 4)
