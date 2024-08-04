45c7b04dc5d258ad5a822d7014d664a88cdb4dcb
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-04 19:16:13 +0700

    Update 7_🚀_Un_15_F(X)_v2.py

--- pages/7_🚀_Un_15_F(X)_v2.py
+++ pages/7_🚀_Un_15_F(X)_v2.py
@@ -213,5 +213,8 @@ sum =    (df_new.cf.values   / min_sum ) * 100
 cf =  df_new.cf.values
 # st.line_chart(cf)
 
-df_all = pd.DataFrame(list(zip(cf, sum , roll_over )) , columns =['Sum.Delta', 'True_Alpha' , 'Max.Sum.Buffer'] )
+df_all = pd.DataFrame(list(zip(cf,   roll_over )) , columns =['Sum.Delta',   'Max.Sum.Buffer'] )
+df_all_2 = pd.DataFrame(list(zip( sum  )) , columns =[ 'True_Alpha' ] )
+
 st.line_chart(df_all)
+st.line_chart(df_all_2)
