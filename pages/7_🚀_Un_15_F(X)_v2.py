384d0950ac6050324bb1112d91ed563d90ca5596
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-04 19:34:41 +0700

    Update 7_🚀_Un_15_F(X)_v2.py

--- pages/7_🚀_Un_15_F(X)_v2.py
+++ pages/7_🚀_Un_15_F(X)_v2.py
@@ -216,6 +216,6 @@ df_all =   pd.DataFrame(list(zip(cf,   roll_over )) , columns =['Sum.Delta',   '
 df_all_2 = pd.DataFrame(  sum , columns = ['True_Alpha']  )
 col1, col2  = st.columns(2)
 
-col2.line_chart(df_all)
-col1.line_chart(df_all_2)
+col1.line_chart(df_all)
+col2.line_chart(df_all_2)
 st.line_chart(df_new)
