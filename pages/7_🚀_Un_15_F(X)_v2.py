ccb6b0996dd9e4ce5f9b7a17ef0dd8db7dba462d
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-04 17:18:23 +0700

    Update 7_🚀_Un_15_F(X)_v2.py

--- pages/7_🚀_Un_15_F(X)_v2.py
+++ pages/7_🚀_Un_15_F(X)_v2.py
@@ -30,5 +30,7 @@ def un_16 (df_pc_pe =['FFWM' , 'NEGG' , 'RIVN' ,'APLS' ]):
   
 Ticker_input  = st.text_input("Ticker", ['FFWM','NEGG','RIVN','APLS'])
 list_from_string = eval(Ticker_input)
-data = un_16(list_from_string)
+# data = un_16(list_from_string)
 # st.line_chart(data)
+
+st.write( list_from_string)
