ef12d6a81e186326e5b123020291347234632559
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-04 17:40:57 +0700

    Update 7_🚀_Un_15_F(X)_v2.py

--- pages/7_🚀_Un_15_F(X)_v2.py
+++ pages/7_🚀_Un_15_F(X)_v2.py
@@ -190,8 +190,8 @@ def un_16 (df_pc_pe =[]):
 Ticker_input  = st.text_input("Ticker", ['FFWM','NEGG','RIVN','APLS'])
 list_from_string = eval(Ticker_input)
 data = un_16(list_from_string)
-drop_df =  [ '{}_re' for i in list_from_string ]
-df_new = data.drop( drop_df , axis=0)
+drop_df =  [ '{}_re'.format(i) for i in list_from_string ]
+df_new = data.drop( drop_df , axis=1)
 st.line_chart(df_new)
 
 
