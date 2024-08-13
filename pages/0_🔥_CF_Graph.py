8e823df9df063d5023bfdd5cd357ec19db441173
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-13 18:25:18 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -60,9 +60,12 @@ def CF_Graph(entry = 1.26 , ref = 1.26 , Fixed_Asset_Value =1500. , Cash_Balan =
 
 st.write(CF_Graph(entry = 3.0)) 
 as_1  = CF_Graph(entry = 3.0).net_pv.values
+as_2  = CF_Graph(entry = 5.0).net_pv.values
 st.write(as_1) 
 
+
 st.plotly_chart( px.line( as_1 ))
+st.plotly_chart( px.line( as_2 ))
 
 
 
