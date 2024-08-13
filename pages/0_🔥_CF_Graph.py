1b67ef7e9026428ce4ad176bdc918c5811b4fba7
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-13 19:18:53 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -57,9 +57,9 @@ def CF_Graph(entry = 1.26 , ref = 1.26 , Fixed_Asset_Value =1500. , Cash_Balan =
             return   df[['Asset_Price' , 'Cash_Balan' , 'net_pv']] ,  df_2[-1]
     except:pass
 
-df.set_index('Asset_Price') ,  df_2 = CF_Graph(entry = 6.88, ref = 5.85)
-# as_1 =  df.set_index('Asset_Price')
-as_1_py = px.line( df )
+df ,  df_2 = CF_Graph(entry = 6.88, ref = 5.85)
+as_1 =  df.set_index('Asset_Price')
+as_1_py = px.line( as_1 )
 as_1_py.add_vline(x= 2.5, line_width=1 , line_dash="dash")
 st.plotly_chart( as_1_py ) 
 st.write( 'rf' , df_2) 
