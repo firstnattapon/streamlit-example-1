8cec6f4ffb4da4c17d0bc320eefc1cbdc03c91e6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2024-08-13 20:00:45 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -104,4 +104,4 @@ with tab5:
     st.plotly_chart( as_1_py ) 
     st.write( 'rf:' , df_APLS) 
 
-st.write( 'sum_rf:' , df_FFWM+df_NEGG+df_RIVN+df_APLS) 
+st.write( 'sum_rf:' , df_FFWM+df_NEGG+df_RIVN+df_APLS , 'asset' = 1500*4 , 'Cash' = 650*4  ) 
