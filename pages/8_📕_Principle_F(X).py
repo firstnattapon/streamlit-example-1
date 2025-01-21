364a702c3c74af8173bfc3fec75d58a1e828a1c1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-01-21 15:39:06 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -1,17 +1,28 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import streamlit_mermaid as stmd
+
 
 st.set_page_config(page_title="Principle_F(X)", page_icon="📕" , layout="wide")
 
-@st.cache_data
-def iframe ():
-  # src="https://www.mindmeister.com/app/map/3178532454?fullscreen=1&v=embedded&m=outline" 
-  # st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
+# @st.cache_data
+# def iframe ():
+#   # src="https://www.mindmeister.com/app/map/3178532454?fullscreen=1&v=embedded&m=outline" 
+#   # st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
 
-  src="https://img.soccersuck.com/images/2025/01/21/brave_screenshot_img.soccersuck.com.png" 
-  st.components.v1.iframe(src, width=1500 , height=800  , scrolling=1)
+#   src="https://img.soccersuck.com/images/2025/01/21/brave_screenshot_img.soccersuck.com.png" 
+#   st.components.v1.iframe(src, width=1500 , height=800  , scrolling=1)
 
-iframe()
+# iframe()
+
+code = """
+graph TD
+A --> B
+B --> C
+C --> A
+"""
+stmd.st_mermaid(code)
+
 st.write('____')
 
 checkbox1 = st.checkbox(' mindmeister' , value=0 )
