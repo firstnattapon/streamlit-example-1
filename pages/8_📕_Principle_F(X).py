ea0697cd9f4cbbc7d06ccbb625ec8d91ddaacd55
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-01-21 17:02:23 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Principle_F(X)", page_icon="📕" , layout="wide"
 def iframe ():
   # src="https://www.mindmeister.com/app/map/3178532454?fullscreen=1&v=embedded&m=outline" 
   # st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
-  src = "https://www.mermaidchart.com/raw/e3003041-c706-467f-b732-e3b1754530d2?theme=light&version=v0.1&format=png"
+  src = "https://img.soccersuck.com/images/2025/01/21/brave_screenshot_www.mermaidchart.com.png"
   st.components.v1.iframe(src, width=1500 , height=800  , scrolling=0)
   
 iframe()
