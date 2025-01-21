3997183cc2fa1e2e7896dcc21ee2c380b9aea9df
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-01-21 22:13:46 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -18,7 +18,7 @@ with tab1:
   
   _, _, _, col4 = st.columns(4)
   with col4:
-    st.image("https://img.soccersuck.com/images/2025/01/21/principle-5-1-1024x666.png", width=300)
+    st.image("https://img.soccersuck.com/images/2025/01/21/principle-5-1-1024x666.png", width=400)
     
   st.link_button("พื้นฐาน Global_macro (Price_Cycle)", "https://drive.google.com/file/d/1-bNM1gPEG7i-CW1TMd_6Cu6Z5132UjGZ/view?usp=sharing")
 
