6431648dc5e2d422f445e16de46808d907bc40b4
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-01-21 12:43:48 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -8,7 +8,7 @@ def iframe ():
   # src="https://www.mindmeister.com/app/map/3178532454?fullscreen=1&v=embedded&m=outline" 
   # st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
   
-  src="https://img.soccersuck.com/images/2025/01/18/02-1.png" 
+  src="https://www.mermaidchart.com/raw/e3003041-c706-467f-b732-e3b1754530d2?theme=light&version=v0.1&format=svg" 
   st.components.v1.iframe(src, width=1500 , height=2000, scrolling=1)
 
 iframe()
