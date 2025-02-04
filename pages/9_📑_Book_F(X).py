37a406bfa4fdd8eff8bec820e8166775a20a4980
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-04 22:47:53 +0700

    Update 9_📑_Book_F(X).py

--- pages/9_📑_Book_F(X).py
+++ pages/9_📑_Book_F(X).py
@@ -13,8 +13,8 @@ def iframe ( frame = ''):
   src = frame
   st.components.v1.iframe(src, width=1500 , height=800  , scrolling=0)
 
-tab1,  tab2, tab3 , tab4 , tab5  , tab6,  tab7, tab8 , tab9 , tab10   = st.tabs(["บทที่_1", "บทที่_2" ,"บทที่_3", "บทที่_4" ,
-                                                                                 "บทที่_5" , "บทที่_6" , "บทที่_7" , "บทที่_8" , "บทที่_9"  , "บทที่_10" ])
+tab1,  tab2, tab3 , tab4 , tab5  , tab6,  tab7, tab8 , tab9 , tab10 ,tab11   = st.tabs(["บทที่_1", "บทที่_2" ,"บทที่_3", "บทที่_4" ,
+                                                                                 "บทที่_5" , "บทที่_6" , "บทที่_7" , "บทที่_8" , "บทที่_9"  , "บทที่_10"  , "บทที่_11" ])
 
 with tab1:
   iframe(frame = "https://monica.im/share/chat?shareId=DeGdfM5eVeodP6Vn")
@@ -54,6 +54,10 @@ with tab9:
 
 with tab10:
   iframe(frame = "https://monica.im/share/chat?shareId=ejez2QpkAaw1n5Rn")
+  st.write('____')
+
+with tab11:
+  iframe(frame = "https://monica.im/share/chat?shareId=ijSatOtgyJ06lElv")
   st.write('____')
 
 
