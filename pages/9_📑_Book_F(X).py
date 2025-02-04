96a159633e18ba556470b9f9ed96acc657f0c671
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-04 20:41:00 +0700

    Update 9_📑_Book_F(X).py

--- pages/9_📑_Book_F(X).py
+++ pages/9_📑_Book_F(X).py
@@ -8,8 +8,16 @@ import numpy as np
 import pandas as pd
 
 st.set_page_config(page_title="Book_F(X)", page_icon="📑" , layout="wide")
+@st.cache_data
+def iframe ( frame = ''):
+  src = frame
+  st.components.v1.iframe(src, width=1500 , height=800  , scrolling=0)
 
 tab1,  Control ,  tab2, tab3 , tab4  = st.tabs(["สรุปบทที่ 1 การคิดระดับสอง", "Control" ,"tab2", "tab3" , "tab4"  ])
 
+
 with tab1:
-  pass
+  iframe(frame = "https://monica.im/share/chat?shareId=DeGdfM5eVeodP6Vn")
+  st.write('____')
+  
+
