3f5adb2f6f9948210b125dbae911a4b91c13f411
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-04 20:39:17 +0700

    Update 9_📑_Book_F(X).py

--- pages/9_📑_Book_F(X).py
+++ pages/9_📑_Book_F(X).py
@@ -9,3 +9,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Book_F(X)", page_icon="📑" , layout="wide")
 
+tab1,  Control ,  tab2, tab3 , tab4  = st.tabs(["สรุปบทที่ 1 การคิดระดับสอง", "Control" ,"tab2", "tab3" , "tab4"  ])
+
+with tab1:
+  pass
