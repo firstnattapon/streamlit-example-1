19b7bbdb947b591a2498f45c63b23dd19dce81a0
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-05 20:18:20 +0700

    Update 9_📑_Book_F(X).py

--- pages/9_📑_Book_F(X).py
+++ pages/9_📑_Book_F(X).py
@@ -13,8 +13,8 @@ def iframe ( frame = ''):
   src = frame
   st.components.v1.iframe(src, width=1500 , height=800  , scrolling=0)
 
-tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10,tab11,tab12,tab13,tab14  = st.tabs(["บทที่_1", "บทที่_2" ,"บทที่_3", "บทที่_4" , "บทที่_5" ,"บทที่_6" , "บทที่_7" , "บทที่_8" , "บทที่_9"  ,
-                                                                                       "บทที่_10"  , "บทที่_11", "บทที่_12", "บทที่_13" , "บทที่_14" ])
+tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10,tab11,tab12,tab13,tab14,tab15  = st.tabs(["บทที่_1", "บทที่_2" ,"บทที่_3", "บทที่_4" , "บทที่_5" ,"บทที่_6" , "บทที่_7" , "บทที่_8" , "บทที่_9"  ,
+                                                                                       "บทที่_10"  , "บทที่_11", "บทที่_12", "บทที่_13" , "บทที่_14", "บทที่_15" ])
 
 with tab1:
   iframe(frame = "https://monica.im/share/chat?shareId=DeGdfM5eVeodP6Vn")
@@ -70,4 +70,8 @@ with tab13:
   
 with tab14:
   iframe(frame = "https://monica.im/share/chat?shareId=TVnfn8gPjit32jXi")
+  st.write('____')
+
+with tab15:
+  iframe(frame = "https://monica.im/share/chat?shareId=hzdSmDqgHikzufVH")
   st.write('____')
