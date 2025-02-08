b25810fdd0a9660469ba92fd1affc723201aa1ef
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-08 16:55:47 +0700

    Update 4.1_🕹_Delta_F(X).py

--- pages/4.1_🕹_Delta_F(X).py
+++ pages/4.1_🕹_Delta_F(X).py
@@ -93,5 +93,9 @@ def plot (Ticker = ''   ,  act = -1 ):
     chart_data = pd.DataFrame(np.array(all).T , columns= np.array(all_id))
     st.line_chart(chart_data)
 
+channel_id = 2385118
+write_api_key = 'IPSG3MMMBJEB9DY8'
+client = thingspeak.Channel(channel_id, write_api_key , fmt='json')
 
-plot( Ticker = 'FFWM'  , act = 1182379  )
+
+plot( Ticker = 'FFWM'  , act =   client.get_field_last(field='{}'.format(2))   )
