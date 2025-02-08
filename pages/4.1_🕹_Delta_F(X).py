2e3352d90bb9704a9f780d92ae3f4215d944d7cb
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-08 16:59:28 +0700

    Update 4.1_🕹_Delta_F(X).py

--- pages/4.1_🕹_Delta_F(X).py
+++ pages/4.1_🕹_Delta_F(X).py
@@ -97,7 +97,6 @@ channel_id = 2385118
 write_api_key = 'IPSG3MMMBJEB9DY8'
 client = thingspeak.Channel(channel_id, write_api_key , fmt='json')
 
-fx = client.get_field_last(field='{}'.format(2))
-fx_js = int(json.loads(fx)["field{}".format(2) ] )
-
-plot( Ticker = 'FFWM'  , act =  fx_js  )
+FFWM_act = client.get_field_last(field='{}'.format(2))
+FFWM_act_js = int(json.loads(fx)["field{}".format(2) ])
+plot( Ticker = 'FFWM'  , act =  FFWM_act_js  )
