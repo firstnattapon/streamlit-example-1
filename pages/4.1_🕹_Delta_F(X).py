aecc15054ef1e584db19d7a07d3cf00d398bc549
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-09 20:35:01 +0700

    Update 4.1_🕹_Delta_F(X).py

--- pages/4.1_🕹_Delta_F(X).py
+++ pages/4.1_🕹_Delta_F(X).py
@@ -83,14 +83,14 @@ def Limit_fx (Ticker = '' , act = -1 ):
     prices = np.array( tickerData.Close.values , dtype=np.float64)
 
     if  act == -1 : # min
-        actions = np.array( np.ones( len(prices) ) , dtype=np.int64).tolist()
+        actions = np.array( np.ones( len(prices) ) , dtype=np.int64) 
 
     elif act == -2:  # max  
-        actions = get_max_action(prices).tolist()
+        actions = get_max_action(prices) 
       
     else :
         rng = np.random.default_rng(act)
-        actions = rng.integers(0, 2, len(prices)).tolist()
+        actions = rng.integers(0, 2, len(prices)) 
     
     # initial_cash = 500.0
     # initial_asset_value = 500.0
@@ -98,59 +98,59 @@ def Limit_fx (Ticker = '' , act = -1 ):
     
     buffer, sumusd, cash, asset_value, amount , refer = calculate_optimized( actions ,  prices)
     
-    df = pd.DataFrame({
-        'price': prices,
-        'action': actions,
-        'buffer': np.round(buffer, 2),
-        'sumusd': np.round(sumusd, 2),
-        'cash': np.round(cash, 2),
-        'asset_value': np.round(asset_value, 2),
-        'amount': np.round(amount, 2),
-        'refer': np.round(refer, 2),
-        'net': np.round( sumusd -  (refer+500) , 2)
-    })
-    return df 
+#     df = pd.DataFrame({
+#         'price': prices,
+#         'action': actions,
+#         'buffer': np.round(buffer, 2),
+#         'sumusd': np.round(sumusd, 2),
+#         'cash': np.round(cash, 2),
+#         'asset_value': np.round(asset_value, 2),
+#         'amount': np.round(amount, 2),
+#         'refer': np.round(refer, 2),
+#         'net': np.round( sumusd -  (refer+500) , 2)
+#     })
+#     return df 
 
-def plot (Ticker = ''   ,  act = -1 ):
-    all = []
-    all_id = []
+# def plot (Ticker = ''   ,  act = -1 ):
+#     all = []
+#     all_id = []
     
-    #min
-    all.append( Limit_fx(Ticker , act = -1 ).net_cf )
-    all_id.append('min')
-    #fx
-    all.append(Limit_fx( Ticker , act = act ).net_cf )
-    all_id.append('fx')
-    #max
-    all.append(Limit_fx( Ticker , act = -2 ).net_cf )
-    all_id.append('max')
+#     #min
+#     all.append( Limit_fx(Ticker , act = -1 ).net_cf )
+#     all_id.append('min')
+#     #fx
+#     all.append(Limit_fx( Ticker , act = act ).net_cf )
+#     all_id.append('fx')
+#     #max
+#     all.append(Limit_fx( Ticker , act = -2 ).net_cf )
+#     all_id.append('max')
     
-    chart_data = pd.DataFrame(np.array(all).T , columns= np.array(all_id))
-    st.line_chart(chart_data)
+#     chart_data = pd.DataFrame(np.array(all).T , columns= np.array(all_id))
+#     st.line_chart(chart_data)
 
-    st.write( Limit_fx(Ticker , act = -2 )  ) 
+#     st.write( Limit_fx(Ticker , act = -2 )  ) 
 
 
-channel_id = 2385118
-write_api_key = 'IPSG3MMMBJEB9DY8'
-client = thingspeak.Channel(channel_id, write_api_key , fmt='json')
+# channel_id = 2385118
+# write_api_key = 'IPSG3MMMBJEB9DY8'
+# client = thingspeak.Channel(channel_id, write_api_key , fmt='json')
 
-FFWM_act = client.get_field_last(field='{}'.format(2))
-FFWM_act_js = int(json.loads(FFWM_act)["field{}".format(2) ])
-plot( Ticker = 'FFWM'  , act =  FFWM_act_js  )
+# FFWM_act = client.get_field_last(field='{}'.format(2))
+# FFWM_act_js = int(json.loads(FFWM_act)["field{}".format(2) ])
+# plot( Ticker = 'FFWM'  , act =  FFWM_act_js  )
 
-NEGG_act = client.get_field_last(field='{}'.format(3))
-NEGG_act_js = int(json.loads(NEGG_act)["field{}".format(3) ])
-plot( Ticker = 'NEGG'  , act =  NEGG_act_js  )
+# NEGG_act = client.get_field_last(field='{}'.format(3))
+# NEGG_act_js = int(json.loads(NEGG_act)["field{}".format(3) ])
+# plot( Ticker = 'NEGG'  , act =  NEGG_act_js  )
 
-RIVN_act = client.get_field_last(field='{}'.format(4))
-RIVN_act_js = int(json.loads(RIVN_act)["field{}".format(4) ])
-plot( Ticker = 'RIVN'  , act =  RIVN_act_js  )
+# RIVN_act = client.get_field_last(field='{}'.format(4))
+# RIVN_act_js = int(json.loads(RIVN_act)["field{}".format(4) ])
+# plot( Ticker = 'RIVN'  , act =  RIVN_act_js  )
 
-APLS_act = client.get_field_last(field='{}'.format(5))
-APLS_act_js = int(json.loads(APLS_act)["field{}".format(5) ])
-plot( Ticker = 'APLS'  , act =  APLS_act_js  )
+# APLS_act = client.get_field_last(field='{}'.format(5))
+# APLS_act_js = int(json.loads(APLS_act)["field{}".format(5) ])
+# plot( Ticker = 'APLS'  , act =  APLS_act_js  )
 
-NVTS_act = client.get_field_last(field='{}'.format(6))
-NVTS_act_js = int(json.loads(NVTS_act)["field{}".format(6) ])
-plot( Ticker = 'NVTS'  , act =  NVTS_act_js  )
+# NVTS_act = client.get_field_last(field='{}'.format(6))
+# NVTS_act_js = int(json.loads(NVTS_act)["field{}".format(6) ])
+# plot( Ticker = 'NVTS'  , act =  NVTS_act_js  )
