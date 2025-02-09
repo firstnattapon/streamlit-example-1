bad472fce41496da4824f15095797e520b40ce33
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-09 22:29:38 +0700

    Update 4_✈️_Delta_F(X).py

--- pages/4_✈️_Delta_F(X).py
+++ pages/4_✈️_Delta_F(X).py
@@ -125,8 +125,8 @@ def plot (Ticker = ''   ,  act = -1 ):
     st.line_chart(chart_data)
 
     df_plot =  Limit_fx(Ticker , act = -1 )
-    st.line_chart( df_plot[['cash']] )
-    st.write( Limit_fx(Ticker , act = act )  ) 
+    st.line_chart( df_plot[['sumusd']] )
+    st.write( Limit_fx(Ticker , act = -1 )  ) 
 
 
 channel_id = 2385118
