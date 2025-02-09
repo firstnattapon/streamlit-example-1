d978ae2cbeafe483cec59e7965ad18ab6fd7224d
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-09 14:44:53 +0700

    Update 4.1_🕹_Delta_F(X).py

--- pages/4.1_🕹_Delta_F(X).py
+++ pages/4.1_🕹_Delta_F(X).py
@@ -46,7 +46,7 @@ def calculate_optimized(actions, prices, cash_start, initial_asset_value, initia
     # net_cf   =   cash   -    refer
     net_cf   =   sumusd   -  (refer + initial_asset_value)
     
-    return buffers, cash, sumusd, refer  , net_cf
+    return buffers, cash, sumusd, refer+500  , net_cf
     
 
 def get_action(prices):
