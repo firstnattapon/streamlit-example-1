13bbbcf1fee67c0f8958b08848e44830033872b3
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-10 20:25:56 +0700

    Update 4_✈️_Limit_F(X).py

--- pages/4_✈️_Limit_F(X).py
+++ pages/4_✈️_Limit_F(X).py
@@ -173,6 +173,7 @@ with Ref_index_Log:
     df_sumusd_['daily_sumusd'] = df_sumusd_.sum(axis=1)
     df_sumusd_['ref_log'] = prices_df
     df_sumusd_['net'] = df_sumusd_['daily_sumusd'] - df_sumusd_['ref_log']
+    df_sumusd_.reset_index(drop=true inplace=true)
     
     st.line_chart(df_sumusd_.net)
     with st.expander("View Raw Data"):
