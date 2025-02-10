9613499ed2b7ed42428a8b9df754ce50a40580e1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-10 22:05:06 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -68,12 +68,12 @@ df_7_3 , fx_js_3  = Monitor(Ticker = 'APLS', field = 5)
 df_7_4 , fx_js_4  = Monitor(Ticker = 'NVTS', field = 6)
 
 nex = 0 
-Nex_day_ = st.checkbox('Nex_day')
+Nex_day_ = st.checkbox('nex_day')
 if Nex_day_ :
-  st.write(nex) 
+  st.write( "value = " , nex) 
   if st.button("Nex_day"):
     nex = 1
-    st.write(nex) 
+    st.write( "value = " , nex) 
 st.write("_____") 
 
 
