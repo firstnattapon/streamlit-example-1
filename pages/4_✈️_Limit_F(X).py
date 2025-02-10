75bfdef59317a72fbfc042a8f28356cff8fcd296
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-10 15:53:47 +0700

    Update 4_✈️_Limit_F(X).py

--- pages/4_✈️_Limit_F(X).py
+++ pages/4_✈️_Limit_F(X).py
@@ -140,10 +140,10 @@ Ref_index_Log ,  Burn_Cash , tab1, tab2, tab3, tab4, tab5 = st.tabs([ 'Ref_index
 with Ref_index_Log:
     STOCK_SYMBOLS = ['FFWM', 'NEGG', 'RIVN', 'APLS']
     buffers = {
-        'buffer_{}'.format(symbol) : Limit_fx(symbol, act=-1).buffer
+        'buffer_{}'.format(symbol) : Limit_fx(symbol, act=-1).sumusd
         for symbol in STOCK_SYMBOLS}
     df_burn_cash = pd.DataFrame(buffers)
-    st.dataframe(df_burn_cash['sumusd'])
+    st.dataframe(df_burn_cash)
 
 
 
