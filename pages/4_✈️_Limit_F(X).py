9f1c50ded5b01d44d1cf52057e3475868edd02ee
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-10 20:27:08 +0700

    Update 4_✈️_Limit_F(X).py

--- pages/4_✈️_Limit_F(X).py
+++ pages/4_✈️_Limit_F(X).py
@@ -173,11 +173,11 @@ with Ref_index_Log:
     df_sumusd_['daily_sumusd'] = df_sumusd_.sum(axis=1)
     df_sumusd_['ref_log'] = prices_df
     df_sumusd_['net'] = df_sumusd_['daily_sumusd'] - df_sumusd_['ref_log']
-    df_sumusd_.reset_index(drop=true  , inplace=true)
+    df_sumusd_.reset_index(  inplace=true)
     
     st.line_chart(df_sumusd_.net)
     with st.expander("View Raw Data"):
-        st.write(df_sumusd_) 
+        st.dataframe(df_sumusd_) 
 
 with Burn_Cash:
     STOCK_SYMBOLS = ['FFWM', 'NEGG', 'RIVN', 'APLS' , 'NVTS' ]
