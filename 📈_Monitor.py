977caf7da3535dc5d758656af3e07d9de4d3752c
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-02-10 21:57:30 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -66,10 +66,12 @@ df_7_1 , fx_js_1  = Monitor(Ticker = 'NEGG', field = 3)
 df_7_2 , fx_js_2  = Monitor(Ticker = 'RIVN', field = 4)
 df_7_3 , fx_js_3  = Monitor(Ticker = 'APLS', field = 5)
 df_7_4 , fx_js_4  = Monitor(Ticker = 'NVTS', field = 6)
+nex = 0 
 
-st.write(df_7)   
-nex = 2
-st.write(df_7.action.values[1+nex])   
+Nex_day = col13.checkbox('@_FFWM_ASSET')
+if st.button("Nex_day"):
+  nex = 1
+st.write("_____") 
 
 
 col13, col16, col14, col15, col17, col18, col19 = st.columns(7)
