f6f89d9364e45893e204181ec84713742ab134a6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-03-08 21:32:09 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -12,7 +12,7 @@ def iframe ( frame = ''):
 tab1,  Control ,  tab2, tab3 , tab4  = st.tabs(["tab1", "Control" ,"tab2", "tab3" , "tab4"  ])
 
 with tab1:
-  iframe(frame = "https://monica.im/share/artifact?id=ijJtUaWKvCzjFNHtZUysRd")    
+  iframe(frame = "https://monica.im/share/artifact?id=ybc5eexwsxVUDh4FbKSwjA")    
   st.link_button("(Price_Cycle) พื้นฐาน Global_macro ", "https://drive.google.com/file/d/1-bNM1gPEG7i-CW1TMd_6Cu6Z5132UjGZ/view?usp=sharing")
  
 
