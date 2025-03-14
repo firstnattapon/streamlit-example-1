0c976e1f60f85c42149e6b03c2a571537db11be1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-03-14 22:48:16 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -34,7 +34,7 @@ with Control:
 with Mind:
   Mind_col1, Mind_col2  = st.columns(2)
   Mind_col1.video('https://www.youtube.com/watch?v=fhgc8FBPnA8')
-  st.components.v1.iframe("https://monica.im/share/artifact?id=wBtgUhDcA94wLY7mD9PmRn", width=1500 , height=800  , scrolling=0)
+  Mind_col2.components.v1.iframe("https://monica.im/share/artifact?id=wBtgUhDcA94wLY7mD9PmRn", width=1500 , height=800  , scrolling=0)
 
 checkbox1 = st.checkbox(' mindmeister' , value=0 )
 if checkbox1 :
