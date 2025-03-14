69338336ac9449dc08cf2ec2cd009115f0f68fc8
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-03-14 22:51:24 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -34,12 +34,10 @@ with Control:
 with Mind:
   Mind_col1, Mind_col2  = st.columns(2)
   Mind_col1.video('https://www.youtube.com/watch?v=fhgc8FBPnA8')
-  Mind_col2.iframe(
-    src="https://monica.im/share/artifact?id=wBtgUhDcA94wLY7mD9PmRn",
+  Mind_col2.components.v1.iframe(src="https://monica.im/share/artifact?id=wBtgUhDcA94wLY7mD9PmRn",
     width=700,  # ปรับขนาดให้เหมาะสมกับ column
     height=400,
-    scrolling=False
-)
+    scrolling=False)
 
 
 
