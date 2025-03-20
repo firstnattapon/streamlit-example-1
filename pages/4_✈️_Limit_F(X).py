88fa6d61aa7764a5ecfaff639e3489ed5a07629c
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-03-20 11:38:29 +0700

    Update 4_✈️_Limit_F(X).py

--- pages/4_✈️_Limit_F(X).py
+++ pages/4_✈️_Limit_F(X).py
@@ -241,7 +241,7 @@ with cf_log:
     st.write ('now_pv' , number)
     st.write ('____')
     st.write ( 'net_cf' , number - (9500 + ln)) ##แก้
-    if st.button("client_log"):
+    if st.button("add_logcf"):
         client_log.update(  {'field3': number - (9500 + ln) } )
     st.write ('____')
     components.iframe('https://thingspeak.mathworks.com/channels/2329127/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15' , width=800, height=200)
