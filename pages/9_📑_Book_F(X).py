4a7a6878e99059fc541b6c4432be31c180fa3491
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-04-10 18:45:20 +0700

    Update 9_📑_Book_F(X).py

--- pages/9_📑_Book_F(X).py
+++ pages/9_📑_Book_F(X).py
@@ -244,6 +244,8 @@ with st.expander("Book : The Most Important Thing : นักลงทุนเ�
     iframe(frame = "https://monica.im/share/chat?shareId=oznm4bVmlX7D61Rn")
     st.write('____')
 
+with st.expander("Book : ทุนนิยมในศตวรรษที่ 21 : Capital in the Twenty-First Century" , expanded = 0 ):
+   st.write('____') 
 
 with st.expander("Book : the power of now : พลังแห่งจิตปัจจุบัน " , expanded = 0 ):
    st.write('____') 
