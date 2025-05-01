370c2359dca0dbfda68eb6f93fd9ed4cbe802c63
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-05-01 10:00:50 +0700

    Update 2_🏠_Add_Gen_F(X).py

--- pages/2_🏠_Add_Gen_F(X).py
+++ pages/2_🏠_Add_Gen_F(X).py
@@ -10,12 +10,13 @@ st.set_page_config(page_title="_Add_Gen_F(X)", page_icon="🏠")
 
 # tickerData = yf.Ticker( 'FFWM')
 # tickerData = round(tickerData.download(period= 'ma' )[['Close']] , 3 )
-recent_data = yf.download("AAPL", period="max")
+multi_data = yf.download(["AAPL", "MSFT"], start="2020-01-01", end="2021-01-01")
+
 
 # tickerData.index = tickerData.index.tz_convert(tz='Asia/bangkok')
 # filter_date = '2023-01-01 12:00:00+07:00'
 # tickerData = tickerData[tickerData.index >= filter_date]
-st.write(recent_data) 
+st.write(multi_data) 
 
 
 @njit(fastmath=True)  # เพิ่ม fastmath=True เพื่อให้ compiler optimize มากขึ้น
