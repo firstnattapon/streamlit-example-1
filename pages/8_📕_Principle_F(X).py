e14ab44670bd72aa858b760be3ec4c9f4aa3de29
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-05-11 16:46:09 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -9,7 +9,7 @@ def iframe ( frame = ''):
   src = frame
   st.components.v1.iframe(src, width=1500 , height=800  , scrolling=0)
 
-Main,  Control , Mind  ,   tab2, tab3 , tab4  , tab5 = st.tabs(["Main", "Control" , "Mind"  ,"tab2", "tab3" , "tab4"  , "tab5" ])
+Main,  Control , Mind  ,   tab2, tab3 , tab4  , tab5 = st.tabs(["Main", "Control" , "Mind"  ,"tab2 (Value vs Time)", "tab3 (Fix_Asset vs Ratio_Asset)" , "tab4 (Principle)"  , "tab5 (Land Price Calculator)" ])
 
 with Main:
   iframe(frame = "https://monica.im/share/artifact?id=9ZkVULFxD58aFRgywPN3Xa")    
