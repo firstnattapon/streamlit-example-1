ac66fc1bca7c384ef02364cf89b8972007101f3e
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-05-11 21:29:03 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -38,7 +38,7 @@ with Mind:
     st.components.v1.iframe('https://monica.im/share/artifact?id=dKModf53bUtZswz2SbrNoJ', width=1500 , height=800  , scrolling=0)
 
 with tab5:
-  iframe(frame = "https://monica.im/share/artifact?id=DGLpUDdFaW7n9VFcr8B9H6")
+  iframe(frame = "https://monica.im/share/artifact?id=DGLpUDdFaW7n9VFcr8B9H6") 
 
 checkbox1 = st.checkbox(' mindmeister' , value=0 )
 if checkbox1 :
