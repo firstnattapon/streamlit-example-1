3f3b96965454609ab2ec4d6ac4ef474615fa5be0
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-05-26 20:29:30 +0700

    Update 6_🛰️_Benchmark_F(X).py

--- pages/6_🛰️_Benchmark_F(X).py
+++ pages/6_🛰️_Benchmark_F(X).py
@@ -153,22 +153,8 @@ def Un_15 (Ticker = '' ):
     di = di.dif.values
     return  a_1 , a_0 , a_3 , di
 
-ans = ['VERV',
- 'SMCI',
- 'RLAY',
- 'MRVI',
- 'DYN',
- 'EVLV',
- 'IOVA',
- 'NVTS',
- 'QS',
- 'GYRE',
- 'AGL',
- 'SFIX',
- 'SYRE',
- 'LAB',
- 'RXRX']
- 
+ans = ['SSII', 'LUNR', 'QXO', 'QUBT', 'BULL', 'SOUN', 'JANX', 'NMAX', 'DJT', 'CLSK']
+
 col1, col2  = st.columns(2)
 number = col2.number_input('Ticker_Yahoo', value=0 , step =1 , min_value=0  ) 
 title = col1.text_input('Ticker_Yahoo', ans[number])
