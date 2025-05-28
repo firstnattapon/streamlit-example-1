19d3a38207b190aee76e2b2ddd6b6e76fdaddaa6
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-05-28 11:12:44 +0700

    Update 1_🔥_Add_CF.py

--- pages/1_🔥_Add_CF.py
+++ pages/1_🔥_Add_CF.py
@@ -31,6 +31,9 @@ APLS_ASSET_LAST = eval(json.loads(APLS_ASSET_LAST)['field4'])
 NVTS_ASSET_LAST = client_2.get_field_last(field='field5')
 NVTS_ASSET_LAST = eval(json.loads(NVTS_ASSET_LAST)['field5'])
 
+QXO_ASSET_LAST = client_2.get_field_last(field='field6')
+QXO_ASSET_LAST = eval(json.loads(QXO_ASSET_LAST)['field6'])
+
 
 def NEGG(entry = 25.20 , ref = 25.20  ):
     try:
@@ -269,11 +272,59 @@ def NVTS(entry = 3.05 , ref = 3.05  ):
     except:pass
 
 
+def QXO(entry = 19.00 , ref = 19.00  ):
+    try:
+        entry  = entry ; step = 0.01 ;  Fixed_Asset_Value = 1500. ; Cash_Balan = 650.
+        if entry < 10000 :
+            samples = np.arange( 0  ,  np.around(entry, 2) * 3 + step  ,  step)
+
+            df = pd.DataFrame()
+            df['Asset_Price'] =   np.around(samples, 2)
+            df['Fixed_Asset_Value'] = Fixed_Asset_Value
+            df['Amount_Asset']  =   df['Fixed_Asset_Value']  / df['Asset_Price']
+
+            df_top = df[df.Asset_Price >= np.around(entry, 2) ]
+            df_top['Cash_Balan_top'] = (df_top['Amount_Asset'].shift(1) -  df_top['Amount_Asset']) *  df_top['Asset_Price']
+            df_top.fillna(0, inplace=True)
+            np_Cash_Balan_top = df_top['Cash_Balan_top'].values
+
+            xx = np.zeros(len(np_Cash_Balan_top)) ; y_0 = Cash_Balan
+            for idx, v_0  in enumerate(np_Cash_Balan_top) :
+                z_0 = y_0 + v_0
+                y_0 = z_0
+                xx[idx] = y_0
+
+            df_top['Cash_Balan_top'] = xx
+            df_top = df_top.rename(columns={'Cash_Balan_top': 'Cash_Balan'})
+            df_top  = df_top.sort_values(by='Amount_Asset')
+            df_top  = df_top[:-1]
+
+            df_down = df[df.Asset_Price <= np.around(entry, 2) ]
+            df_down['Cash_Balan_down'] = (df_down['Amount_Asset'].shift(-1) -  df_down['Amount_Asset'])     *  df_down['Asset_Price']
+            df_down.fillna(0, inplace=True)
+            df_down = df_down.sort_values(by='Asset_Price' , ascending=False)
+            np_Cash_Balan_down = df_down['Cash_Balan_down'].values
+
+            xxx= np.zeros(len(np_Cash_Balan_down)) ; y_1 = Cash_Balan
+            for idx, v_1  in enumerate(np_Cash_Balan_down) :
+                z_1 = y_1 + v_1
+                y_1 = z_1
+                xxx[idx] = y_1
+
+            df_down['Cash_Balan_down'] = xxx
+            df_down = df_down.rename(columns={'Cash_Balan_down': 'Cash_Balan'})
+            df = pd.concat([df_top, df_down], axis=0)
+            df['net_pv'] = df['Fixed_Asset_Value'] + df['Cash_Balan']
+            df =  df [df['Asset_Price'] == np.around(ref, 2) ]['net_pv'].values
+            return    df[-1]
+    except:pass
+
 x_1 = st.number_input('ราคา_NEGG_1.26 , 25.20' , step=0.01 ,  value =  yf.Ticker('NEGG').fast_info['lastPrice']   ) 
 x_2 = st.number_input('ราคา_FFWM_6.88', step=0.01  ,  value = yf.Ticker('FFWM').fast_info['lastPrice']   ) 
 x_3 = st.number_input('ราคา_RIVN_10.07', step=0.01 ,   value = yf.Ticker('RIVN').fast_info['lastPrice'] ) 
 x_4 = st.number_input('ราคา_APLS_39.61', step=0.01 ,   value = yf.Ticker('APLS').fast_info['lastPrice'] ) 
 x_5 = st.number_input('ราคา_NVTS_3.05', step=0.01, value=yf.Ticker('NVTS').fast_info['lastPrice'])
+x_6 = st.number_input('ราคา_NVTS_19.00', step=0.01, value=yf.Ticker('QXO').fast_info['lastPrice'])
 
 st.write("_____") 
 
@@ -301,6 +352,10 @@ y_5 = st.number_input('NVTS_asset', step=0.01, value= NVTS_ASSET_LAST)
 y_5 = y_5 * x_5
 st.write(y_5)
 
+y_6 = st.number_input('QXO_asset', step=0.01, value= QXO_ASSET_LAST)
+y_6 = y_6 * x_6
+st.write(y_6)
+
 j_1 = st.number_input('Portfolio_cash', step=0.01 , value = 0.00  )
 st.write("_____") 
 
@@ -309,9 +364,10 @@ q_2 =  FFWM( ref = x_2 )
 q_3 =  RIVN( ref = x_3 )
 q_4 =  APLS( ref = x_4 )
 q_5 =  NVTS( ref = x_5 )
+q_6 =  QXO( ref = x_6 )
 
-k_1 = (y_1 + y_2 + y_3 + y_4 + y_5) + j_1
-k_2 = (q_1 + q_2 + q_3 + q_4 + q_5) + z_1
+k_1 = (y_1 + y_2 + y_3 + y_4 + y_5 + y_6) + j_1
+k_2 = (q_1 + q_2 + q_3 + q_4 + q_5 + q_6) + z_1
 k_3 =  10750 + z_1
 
 ref = k_2
