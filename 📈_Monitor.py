94eefdd118273d0cd5182760492f57325bd7a4cd
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-03 11:36:08 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -336,8 +336,9 @@ if Limut_Order_NVTS:
             col_nvts6.write(NVTS_ASSET_LAST  + u8)
             
     fix = 2100 ; diff = {"buy": 60,"sell": -60} ; asset = 361 ; fx = lambda fix, diff_value, asset: (fix + diff_value) / asset
+    nvts_M = { 'buy' : fx(fix, diff['buy'], asset) , 'Price':  yf.Ticker('NVTS').fast_info['lastPrice']   ,  'sell' :   fx(fix, diff['sell'], asset) }
 
-    st.write( 'buy' ,  fx(fix, diff['buy'], asset) , yf.Ticker('NVTS').fast_info['lastPrice'] ,  fx(fix, diff['sell'], asset)   )
+    st.write(nvts_M )
 
 st.write("_____")
 
