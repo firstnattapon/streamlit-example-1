20aa199c5194abac2cfbc24e94049632dbd3a067
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-03 11:20:58 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -334,8 +334,9 @@ if Limut_Order_NVTS:
         if GO_NVTS_Buy:
             client.update({'field5': NVTS_ASSET_LAST + u8})
             col_nvts6.write(NVTS_ASSET_LAST  + u8)
-    
-    st.write("ฟฟฟ")
+            
+    fix = 2100 ; diff = [60 , -60] ; asset = 300 ; fx = lambda fix , diff[0], x, asset : (fix + diff) / asset
+    st.write(fx  )
 st.write("_____")
 
 Limut_Order_QXO = st.checkbox('Limut_Order_QXO', np.where(Nex_day_sell == 1, toggle(df_7_5.action.values[1+nex]), df_7_5.action.values[1+nex]))
