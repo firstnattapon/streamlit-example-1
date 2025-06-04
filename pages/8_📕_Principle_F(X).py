848b914f049dd4f018aa8f1922fcfc4629921d5f
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-04 13:04:35 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -12,7 +12,7 @@ def iframe ( frame = ''):
 Main,  Control , Mind  ,   tab2, tab3 , tab4  , tab5 = st.tabs(["Main", "Control" , "Mind"  ,"tab2 (Value vs Time)", "tab3 (Fix_Asset vs Ratio_Asset)" , "tab4 (Principle)"  , "tab5 (Land Price Calculator)" ])
 
 with Main:
-  iframe(frame = "https://monica.im/share/artifact?id=AfmaCL6959oJZxf3CE6RGE")    
+  iframe(frame = "https://monica.im/share/artifact?id=nrFWXCsa3Bwf3EvM74DwU7")    
   st.link_button("(Price_Cycle) พื้นฐาน Global_macro ", "https://drive.google.com/file/d/1-bNM1gPEG7i-CW1TMd_6Cu6Z5132UjGZ/view?usp=sharing")
  
 with tab2:
