27b3d29ae46dc070337349e3b1e349387b6ac24e
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-04 14:07:11 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -30,8 +30,8 @@ st.write('____')
 
 with Control:
   st.link_button("1 .หลักการและคำตอบว่า = ทำไม FIX = LN(T0/TN) ใช้ได้", "https://drive.google.com/file/d/1x_zlS7y9tTxWxHqD_yWpqrvFkGH2J2pB/view?usp=sharing")
-  st.link_button("2 .เครื่องมือประมาณราคาที่ดิน", "https://monica.im/share/artifact?id=McLUV4GWRWenzAaWWx44qk" )
-  iframe(frame = "https://monica.im/share/artifact?id=DGLpUDdFaW7n9VFcr8B9H6")
+  st.link_button("2 .เครื่องมือประมาณราคาที่ดิน", "https://monica.im/share/artifact?id=DGLpUDdFaW7n9VFcr8B9H6" )
+  iframe(frame = "https://monica.im/share/artifact?id=McLUV4GWRWenzAaWWx44qk")
 
 with Mind:
   with st.expander("คลิป"):
