5a78fb449f86e24b879bcd4192bd20d9b397bb8e
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-14 21:17:32 +0700

    Update Calculator.py

--- pages/Calculator.py
+++ pages/Calculator.py
@@ -169,7 +169,7 @@ def main():
         # prod_cost_display = f"{prod_cost[0]:.2f}" if prod_cost is not None else "N/A"
         
         st.write(ticker)
-        st.write(f"f(x): {fx_js} ,  , Production: max")
+        st.write(f"f(x): {fx_js} ,   Production: max")
         st.table(df_7)
         st.write("_____")
 
