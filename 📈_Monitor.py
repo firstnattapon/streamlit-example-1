ffc7980e6e61abcd5762590663bfcf8870a6f585
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-21 21:03:32 +0700

    Update 📈_Monitor.py

--- 📈_Monitor.py
+++ 📈_Monitor.py
@@ -11,7 +11,7 @@ import concurrent.futures
 from threading import Lock
 import os
 
-st.set_page_config(page_title="Monitor", page_icon="📈", layout="wide" , initial_sidebar_state  = "expanded" )
+st.set_page_config(page_title="Monitor", page_icon="📈", layout="wide"  )
 
 # ---------- CONFIGURATION ----------
 @st.cache_data
