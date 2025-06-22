1405040391fa6db954d53950c6812e8943b76a7b
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-22 21:46:27 +0700

    Update 4_🧬_Hybrid_Multi_Mutation.py

--- pages/4_🧬_Hybrid_Multi_Mutation.py
+++ pages/4_🧬_Hybrid_Multi_Mutation.py
@@ -26,8 +26,8 @@ def load_config(filepath: str = "dynamic_seed_config.json") -> Dict[str, Any]:
         "assets": ["FFWM", "NEGG", "RIVN", "AGL", "APLS", "FLNC", "NVTS" , "QXO" ,"RXRX"],
         "default_settings": { 
             "selected_ticker": "FFWM", "start_date": "2024-01-01", 
-            "window_size": 30, "num_seeds": 10000, "max_workers": 8,
-            "mutation_rate": 5.0, "num_mutations": 2 
+            "window_size": 30, "num_seeds": 1000, "max_workers": 8,
+            "mutation_rate": 10.0, "num_mutations": 2 
         }
     }
 
