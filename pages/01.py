44999bc7e455bff69b65fae293b4b028211d528d
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-06-23 16:06:47 +0700

    Update 01.py

--- pages/01.py
+++ pages/01.py
@@ -88,7 +88,7 @@ def average_cf(cf_config):
     history = get_ticker_history(cf_config['ticker'])
     default_date = "2024-01-01 12:00:00+07:00"
     filter_date = cf_config.get('filter_date', default_date)
-    filtered_data = history[history.index > filter_date]
+    filtered_data = history[history.index >= filter_date]
     count_data = len(filtered_data)
     if count_data == 0:
         return 0
