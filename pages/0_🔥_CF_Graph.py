76037e8b7371772c2f77463c6900561ff7117c94
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-08-10 22:37:51 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -6,7 +6,7 @@ import json
 import plotly.express as px
 
 # ตั้งค่าหน้าเว็บ
-st.set_page_config(page_title="CF_Graph", page_icon="🔥" , layout= "centered" )
+st.set_page_config(page_title="CF_Graph", page_icon="🔥" , layout= "wide" )
 
 # --- ฟังก์ชันหลักในการคำนวณ (เหมือนเดิมทุกประการ) ---
 def CF_Graph(entry=1.26, ref=1.26, Fixed_Asset_Value=1500., Cash_Balan=650.):
