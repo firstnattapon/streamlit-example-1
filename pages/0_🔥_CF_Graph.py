96cd8a2a7fb01887bca4e846fd09e227dcb0be71
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-08-10 22:42:44 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -94,7 +94,7 @@ with tabs[1]:
     with st.expander("หลักการ BATA" , expanded=True):
         st.components.v1.iframe("https://monica.im/share/artifact?id=YiXmjHQbwXcXEkhJMoMKTo", width=1500 , height=1000  , scrolling=0)
     with st.expander("หลักการ Rollover"):
-        st.components.v1.iframe("https://monica.im/share/artifact?id=E9Mg5JX9RaAcfssZsU7K3E", width=1100 , height=1000  , scrolling=0)
+        st.components.v1.iframe("https://monica.im/share/artifact?id=E9Mg5JX9RaAcfssZsU7K3E", width=1500 , height=1000  , scrolling=0)
 
 # 3. สร้าง Tab "DATA" สำหรับรับ Input
 with tabs[0]:
