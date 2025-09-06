0aaea7da8afa91c94711575b9ddaf90b8528a2f2
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-09-06 16:55:14 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -34,7 +34,7 @@ with tabs[3]:
 
 with tabs[4]:
     with st.expander("Historical_Backtest_CF", expanded=True):
-        st.components.v1.iframe("https://monica.im/share/artifact?id=aYyGrPmCRG24qMZiB4872K", width=1500, height=1000, scrolling=0)
+        st.components.v1.iframe("https://monica.im/share/artifact?id=fLyda7yGeP4GNz66yaY3JK", width=1500, height=1000, scrolling=0)
 
 
 def parse_final_two_numbers(s):
