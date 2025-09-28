87e0a6cd1fe311236836a37544b3b2abdea18b06
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-09-28 20:56:08 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -38,7 +38,7 @@ with tabs[4]:
         
 with tabs[5]:
     with st.expander("3_piecewise_line", expanded=True):
-        st.components.v1.iframe("https://monica.im/share/artifact?id=zVAQJs3QChrTmgiHBJZmin", width=1500, height=1000, scrolling=0)
+        st.components.v1.iframe("https://monica.im/share/artifact?id=t8qKXjs8Aywi3PcTf4pDZM", width=1500, height=1000, scrolling=0)
 
 def parse_final_two_numbers(s):
     nums = re.findall(r"[-+]?\d*\.?\d+", str(s))
