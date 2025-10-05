4fb29f5c3a9ae7c6058a5004ddfc3b1737c5db81
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-10-05 19:59:11 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -39,7 +39,7 @@ with tabs[4]:
 with tabs[5]:
     with st.expander("3_piecewise_line", expanded=True):
         st.components.v1.iframe("https://monica.im/share/artifact?id=LyE8aUzDvnNGDoAf96YvFA", width=1500, height=1000, scrolling=0)
-        with st.expander("3_piecewise_line_v1", expanded=True):
+        with st.expander("3_piecewise_line_v1", expanded=False):
             st.components.v1.iframe("https://monica.im/share/artifact?id=t8qKXjs8Aywi3PcTf4pDZM", width=1500, height=1000, scrolling=0)
 
 def parse_final_two_numbers(s):
