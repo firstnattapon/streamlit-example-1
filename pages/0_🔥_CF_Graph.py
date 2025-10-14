369d2be93bfcfa52297d7681e0cc8440da224de1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-10-14 23:15:21 +0700

    Update 0_🔥_CF_Graph.py

--- pages/0_🔥_CF_Graph.py
+++ pages/0_🔥_CF_Graph.py
@@ -36,7 +36,7 @@ with tabs[4]:
     with st.expander("Historical_Backtest_CF", expanded=True):
         st.components.v1.iframe("https://monica.im/share/artifact?id=sjbADLCcnqXKFmvVPCL3fA", width=1500, height=1000, scrolling=0)
         with st.expander("Monthly", expanded=False):        
-            st.components.v1.iframe("https://monica.im/share/artifact?id=4QKEe8nn6WNsWvx4LQaa77", width=1500, height=1000, scrolling=0)
+            st.components.v1.iframe("https://monica.im/share/artifact?id=cgYnircvQqM9YUf58VHd8a", width=1500, height=1000, scrolling=0)
         
 with tabs[5]:
     with st.expander("3_piecewise_line", expanded=True):
