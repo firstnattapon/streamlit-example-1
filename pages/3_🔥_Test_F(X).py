0e0d3e533fc8fa71f5cf85fd5ea706dafdac4cfa
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2025-10-17 22:11:57 +0700

    Update 3_🔥_Test_F(X).py

--- pages/3_🔥_Test_F(X).py
+++ pages/3_🔥_Test_F(X).py
@@ -285,16 +285,11 @@ def display_results(
         col1.metric('log_pv Baseline (Sum of fix_c)', f"{sum_fix_c:,.2f}")
         col2.metric('log_pv Adjustment (ln_weighted)', f"{lnw:,.2f}")
 
+        col3, col4 , col5  = st.columns(3)
         # Final value (now_pv)
-        st.metric(label="Now_pv", value=f"{now_pv:,.2f}")
-        
-        st.metric(label="-", value="-")
-
-        
-        st.metric(
-            f"Log PV (Calculated: {sum_fix_c:,.2f} + {lnw:,.2f})",
-            f"{log_pv:,.2f}"
-        )
+        col3.metric(label="Now_pv", value=f"{now_pv:,.2f}")
+        col4.metric(label="", value="-")
+        col5.metric(f"Log PV (Calculated: {sum_fix_c:,.2f} + {lnw:,.2f})",f"{log_pv:,.2f}")
 
         st.metric(label="💰 Net CF (Combined)", value=f"{net_cf:,.2f}")
 
