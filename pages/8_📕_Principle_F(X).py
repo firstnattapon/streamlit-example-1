a7ffe0fc107016cae763b21c61030a12c19d02b0
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2026-01-27 21:14:24 +0700

    Update 8_📕_Principle_F(X).py

--- pages/8_📕_Principle_F(X).py
+++ pages/8_📕_Principle_F(X).py
@@ -39,7 +39,7 @@ with Mind:
     st.components.v1.iframe('https://monica.im/share/artifact?id=dKModf53bUtZswz2SbrNoJ', width=1500 , height=800  , scrolling=0)
 
 with tab5:
-  iframe(frame = "https://monica.im/share/artifact?id=P27uB3GoYVYa9t6axBPyz7")  
+  iframe(frame = "https://monica.im/share/artifact?id=S2wVKpZe5GL8hjyZBzZXtS")  
 
 with tab6:
   iframe(frame = "https://monica.im/share/artifact?id=5UJtZtSGPoRpY5ZAGyU6mY")  
